22d2099803018e494c193d9cb25c1530dcf3c652
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2012-12-14 15:15:46 -0500

    Restructured sloshing experiment directory
    Added sloshing/common, sloshing/layer and sloshing/rho to facilitate testing of different coordinates side-by-side.
    
    This is a prerequisite to addressing IssueID #973.

--- examples/sloshing/mk_plots.m
+++ examples/sloshing/mk_plots.m
@@ -1,20 +1,4 @@
-nc=netcdf('MOM_IC.nc');
-x=nc{'lonh'}(:);
-eta=squeeze( nc{'eta'}(:,1,:) );
-salt=squeeze( nc{'Salt'}(1,:,1,:) );
-gcolor(salt,eta,x); colorbar
-xlabel(sprintf('X (%s)',nc{'lonh'}.units(:)))
-ylabel(sprintf('Z (%s)',nc{'eta'}.units(:)))
-title(sprintf('%s (%s)',nc{'Salt'}.long_name(:),nc{'Salt'}.units(:)))
-close(nc)
-print -dpng Fig_a.png
-
-nc=netcdf('prog.nc');
-eta=squeeze( nc{'e'}(10,:,1,:) );
-salt=squeeze( nc{'salt'}(10,:,1,:) );
-gcolor(salt,eta,x); colorbar
-xlabel(sprintf('X (%s)',nc{'xh'}.units(:)))
-ylabel(sprintf('Z (%s)',nc{'e'}.units(:)))
-title(sprintf('%s (%s)',nc{'salt'}.long_name(:),nc{'salt'}.units(:)))
-close(nc)
-print -dpng Fig_b.png
+subplot(221);gsection('layer/MOM_IC.nc','Salt',1,1,:);
+subplot(222);gsection('rho/MOM_IC.nc','Salt',1,1,:);
+subplot(223);gsection('layer/prog.nc','salt',inf,1,:);
+subplot(224);gsection('rho/prog.nc','salt',inf,1,:);
