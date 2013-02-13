39ec5c949799c7fb8c697670a52981af29e33f3f
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2013-02-13 12:41:41 -0500

    Moved remaining regridding pars out of regrid_defs
    All the remaining parameter constants in regrid_defs that are
    used only in MOM_regridding.F90 have been from into the latter.

--- src/ALE/MOM_regridding.F90
+++ src/ALE/MOM_regridding.F90
@@ -86,6 +86,39 @@ public pressure_gradient_plm
 public pressure_gradient_ppm
 
 ! -----------------------------------------------------------------------------
+! The following are private constants
+! -----------------------------------------------------------------------------
+! List of interpolation schemes
+integer, parameter :: INTERPOLATION_P1M_H2     = 0 ! O(h^2)
+integer, parameter :: INTERPOLATION_P1M_H4     = 1 ! O(h^2)
+integer, parameter :: INTERPOLATION_P1M_IH4    = 2 ! O(h^2)
+integer, parameter :: INTERPOLATION_PLM        = 3 ! O(h^2)
+integer, parameter :: INTERPOLATION_PPM_H4     = 4 ! O(h^3)
+integer, parameter :: INTERPOLATION_PPM_IH4    = 5 ! O(h^3)
+integer, parameter :: INTERPOLATION_P3M_IH4IH3 = 6 ! O(h^4)
+integer, parameter :: INTERPOLATION_P3M_IH6IH5 = 7 ! O(h^4)
+integer, parameter :: INTERPOLATION_PQM_IH4IH3 = 8 ! O(h^4)
+integer, parameter :: INTERPOLATION_PQM_IH6IH5 = 9 ! O(h^5)
+
+! List of interpolant degrees
+integer, parameter :: DEGREE_1 = 1, DEGREE_2 = 2, DEGREE_3 = 3, DEGREE_4 = 4
+
+! Maximum number of regridding iterations
+integer, parameter :: NB_REGRIDDING_ITERATIONS = 1
+! Deviation tolerance between succesive grids in regridding iterations
+real, parameter    :: DEVIATION_TOLERANCE = 1e-10
+! Maximum number of Newton-Raphson iterations. Newton-Raphson iterations are
+! used to build the new grid by finding the coordinates associated with 
+! target densities and interpolations of degree larger than 1.
+integer, parameter :: NR_ITERATIONS = 8
+! Tolerance for Newton-Raphson iterations (stop when increment falls below this)
+real, parameter    :: NR_TOLERANCE = 1e-12
+! When the N-R algorithm produces an estimate that lies outside [0,1], the
+! estimate is set to be equal to the boundary location, 0 or 1, plus or minus
+! an offset, respectively, when the derivative is zero at the boundary.
+real, parameter    :: NR_OFFSET = 1e-6
+
+! -----------------------------------------------------------------------------
 ! This module contains the following routines
 ! -----------------------------------------------------------------------------
 contains
