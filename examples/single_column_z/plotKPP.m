056898f0ef5ef1493a9a97f9857cde160e2f5f78
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2013-08-24 16:44:16 -0400

    Added single_column_z experiment
    o This is similar to single_column but uses KPP and z* coordinates.
    o Changed diagnostic frequency to 2 hours.
    o Added matlab scripts for standard plots.

--- examples/single_column_z/plotKPP.m
+++ examples/single_column_z/plotKPP.m
@@ -1,7 +1,7 @@
 ZLIM=[-250 0];
 interpcolormap('bgr')
 
-ncload('prog.nc'); ncload('visc.nc');
+ncload('prog.nc'); ncload('visc.nc')
 % Check that the state is horizontally uniform
 dt = abs(temp(:,:,2,1) - temp(:,:,1,1));
 dt = dt + abs(temp(:,:,1,2) - temp(:,:,1,1));
@@ -26,22 +26,22 @@ gcolor(temp',e',Time);ylim(ZLIM);colorbar
 caxis([18 21])
 xlabel('Time (days)');ylabel('z (m)')
 title('\theta (^oC)')
-hold on;plot(Time,-h_ML,'w');hold off
+hold on;plot(Time,-KPP_OBLdepth,'w');hold off
 
 subplot(322)
 gcolor(salt',e',Time);ylim(ZLIM);colorbar
 caxis([36 37])
 xlabel('Time (days)');ylabel('z (m)')
 title('S (ppt)')
-hold on;plot(Time,-h_ML,'w');hold off
+hold on;plot(Time,-KPP_OBLdepth,'w');hold off
 
 subplot(323)
 gcolor(KPP_N',e',Time);ylim(ZLIM);colorbar
-stats(KPP_N,'KPP N')
+stats(KPP_N,'KPP_N')
 caxis([0 2e-2])
 xlabel('Time (days)');ylabel('z (m)')
 title('KPP N (1/s)')
-hold on;plot(Time,-h_ML,'w');hold off
+hold on;plot(Time,-KPP_OBLdepth,'w');hold off
 
 subplot(324)
 gcolor(Kd_interface',e',Time);ylim(ZLIM);colorbar
@@ -49,7 +49,7 @@ stats(Kd_interface,'Kd_interface')
 caxis([0 2e-2])
 xlabel('Time (days)');ylabel('z (m)')
 title('Kd interface (m^2/s)')
-hold on;plot(Time,-h_ML,'w');hold off
+hold on;plot(Time,-KPP_OBLdepth,'w');hold off
 
 subplot(325)
 gcolor(KPP_Ksalt',e',Time);ylim(ZLIM);colorbar
