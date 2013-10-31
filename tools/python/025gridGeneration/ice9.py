bceec1f54350038474e2ae761caa828db31e6458
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2013-10-31 18:24:44 -0400

    Made nEdits a static dimension in ice9
    o fms couldn't read topography from a file with an unlimited dimension
      in another variable!

--- tools/python/025gridGeneration/ice9.py
+++ tools/python/025gridGeneration/ice9.py
@@ -47,36 +47,61 @@ def main():
 
 def applyIce9(fileName, nFileName, variable, x0, y0, shallow, analyze):
 
-  if not fileName==nFileName: sh.copyfile(fileName,nFileName)
-
-  # Open netcdf file
-  try: rg=Dataset( nFileName, 'a' );
-  except: error('There was a problem opening "'+nFileName+'".')
-  dir(rg)
-
-  rgDepth = rg.variables[variable] # handle to the variable
-  dims = rgDepth.dimensions # tuple of dimensions
-  depth = rgDepth[:] # Read the data
-  #rg.renameVariable('depth','original_depth')
+  iRg = Dataset( fileName, 'r' );
+  iDepth = iRg.variables[variable] # handle to the variable
+  depth = iDepth[:] # Read the data
   print 'Range of input depths: min=',np.amin(depth),'max=',np.amax(depth)
+
+  # Open new netcdf file
+  if fileName==nFileName: error('Output file must be different from the input file')
+  try: rg=Dataset( nFileName, 'w', format='NETCDF3_CLASSIC' );
+  except: error('There was a problem opening "'+nFileName+'".')
+
+  (ny, nx) = depth.shape
+  rg.createDimension('nx',nx)
+  rg.createDimension('ny',ny)
+  rgDepth = rg.createVariable('depth','f4',('ny','nx'))
+  rgDepth.units = iDepth.units
+  rgDepth.standard_name = iDepth.standard_name
+  rgDepth.description = 'Non-negative nominal thickness of the ocean at cell centers'
+  rg.createDimension('ntiles',1)
 
   # A mask based solely on value of depth
   #notLand = np.where( depth<0, 1, 0)
   notLand = ice9it(600,270,depth)
-  rgWet = rg.createVariable('wet','f4',dims)
+
+  rgWet = rg.createVariable('wet','f4',('ny','nx'))
   rgWet.long_name = 'Wet/dry mask'
   rgWet.description = 'Values: 1=Ocean, 0=Land'
   rgWet[:] = notLand # + (1-notLand)*0.3*np.where( depth<0, 1, 0)
 
-  rgMod = rg.createVariable('modified_mask','f4',dims)
-  rgMod.long_name = 'Modified mask'
-  rgMod.description = 'Values: 1=Ocean, 0=Land, -1 indicates water points removed by "Ice 9" algorithm. 2 indicates wet points that are shallower than 1m deep.'
-  rgMod[:] = notLand - (1-notLand)*np.where( depth<0, 1, 0) + np.where( (notLand>0) & (depth>-shallow), 1, 0)
+  rgDepth[:] = -depth*notLand # Change sign here. Until this point depth has actually been elevation.
 
-  #zero = np.where( depth<0 & notLand==0, 1, 0)
-  #nDepth = rg.createVariable('depth','f4',dims)
-  rgDepth[:] = depth*notLand
-  rg.sync()
+  if 'std' in iRg.variables: # Need to copy over list of edits
+    rgH2 = rg.createVariable('h2','f4',('ny','nx'))
+    rgH2.units = iDepth.units+'^2'
+    rgH2.standard_name = 'Variance of sub-grid scale topography'
+    rgH2[:] = iRg.variables['std'][:]**2
+
+  if 'zEdit' in iRg.variables: # Need to copy over list of edits
+    rgMod = rg.createVariable('modified_mask','f4',('ny','nx'))
+    rgMod.long_name = 'Modified mask'
+    rgMod.description = 'Values: 1=Ocean, 0=Land, -1 indicates water points removed by "Ice 9" algorithm. 2 indicates wet points that are shallower than 1m deep.'
+    rgMod[:] = notLand - (1-notLand)*np.where( depth<0, 1, 0) + np.where( (notLand>0) & (depth>-shallow), 1, 0)
+    n = len(iRg.variables['zEdit'])
+    nEd = rg.createDimension('nEdits',n)
+    iEd = rg.createVariable('iEdit','i4',('nEdits',))
+    iEd.long_name = 'i-index of edited data'
+    jEd = rg.createVariable('jEdit','i4',('nEdits',))
+    jEd.long_name = 'j-index of edited data'
+    zEd = rg.createVariable('zEdit','f4',('nEdits',))
+    zEd.long_name = 'Original value of height data'
+    zEd.units = iDepth.units
+    iEd[:] = iRg.variables['iEdit'][:]
+    jEd[:] = iRg.variables['jEdit'][:]
+    zEd[:] = iRg.variables['zEdit'][:]
+
+  rg.close()
   print 'File "%s" written.'%(nFileName)
 
   # Analyze the shallow points
