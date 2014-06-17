349c31c66199f9414a0f7e480088eeb352329aa3
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2014-06-17 17:12:55 -0400

    Scripts needed for new OM4 grid and data
    - Updated targets in Makefile
    - interpSaltRestore.py to generate salt restoration file
    - interpTides.py to create tidal amplitudes file
    - interpCHL.py to create SeaWIFS chl_a file
    - make_basin_mask.py to create basin codes

---
+++ tools/python/025gridGeneration/interpSaltRestore.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from midas import *
+sgrid=supergrid(file='ocean_hgrid.nc')
+grid=rectgrid(supergrid=sgrid)
+O=state('/archive/gold/datasets/obs/WOA05_pottemp_salt.nc',fields=['SALT'],z_indices=np.arange(0,1),default_calendar='noleap')
+O.var_dict['SALT']['Z'] = None
+OM=O.horiz_interp('SALT',target=grid)
+OM.grid.D=nc.Dataset('ocean_topog.nc').variables['depth'][:]
+OM.grid.wet=np.zeros(OM.grid.D.shape)
+OM.grid.wet[OM.grid.D>0.]=1.
+OM.fill_interior('SALT',smooth=True,num_pass=100)
+OM.mask_where('SALT','grid.D<=0.')
+OM.rename_field('SALT','salt')
+OM.var_dict['salt']['xax_data']=grid.x_T[0,:]
+OM.var_dict['salt']['yax_data']=grid.y_T[:,grid.im/4]
+OM.write_nc('salt_restore.nc',fields=['salt'])
