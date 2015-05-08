69a62bae8c94df10dc511d9a3699d346e5ff885f
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2015-05-08 14:23:29 -0400

    Bugfix: allocate Kv_turb if using the new EPBL
    - visc%Kv_turb is now conditionally allocated depending whether
      any of Jackson et al., KPP or EPBL are being used.
    - Previously, the EPBL was missing from that list.
    - No answer changes.

--- src/parameterizations/vertical/MOM_set_viscosity.F90
+++ src/parameterizations/vertical/MOM_set_viscosity.F90
@@ -1498,24 +1498,27 @@ subroutine set_visc_register_restarts(G, param_file, visc, restart_CS)
 !                   fields.  Allocated here.
 !  (in)      restart_CS - A pointer to the restart control structure.
   type(vardesc) :: vd
-  logical :: use_kappa_shear, adiabatic, useKPP
+  logical :: use_kappa_shear, adiabatic, useKPP, useEPBL
   integer :: isd, ied, jsd, jed, nz
   character(len=40)  :: mod = "MOM_set_visc"  ! This module's name.
   isd = G%isd ; ied = G%ied ; jsd = G%jsd ; jed = G%jed ; nz = G%ke
 
   call get_param(param_file, mod, "ADIABATIC", adiabatic, default=.false., &
                  do_not_log=.true.)
-  use_kappa_shear = .false. ; useKPP = .false.
+  use_kappa_shear = .false. ; useKPP = .false. ; useEPBL = .false.
   if (.not.adiabatic) then
     use_kappa_shear = kappa_shear_is_used(param_file)
     call get_param(param_file, mod, "USE_KPP", useKPP, &
                  "If true, turns on the [CVmix] KPP scheme of Large et al., 1984,\n"// &
                  "to calculate diffusivities and non-local transport in the OBL.", &
                  default=.false., do_not_log=.true.)
+    call get_param(param_file, mod, "ENERGETICS_SFC_PBL", useEPBL, &
+                 "If true, use an implied energetics planetary boundary \n"//&
+                 "layer scheme to determine the diffusivity and viscosity \n"//&
+                 "in the surface boundary layer.", default=.false., do_not_log=.true.)
   endif
 
-
-  if (use_kappa_shear .or. useKPP) then
+  if (use_kappa_shear .or. useKPP .or. useEPBL) then
     allocate(visc%Kd_turb(isd:ied,jsd:jed,nz+1)) ; visc%Kd_turb(:,:,:) = 0.0
     allocate(visc%TKE_turb(isd:ied,jsd:jed,nz+1)) ; visc%TKE_turb(:,:,:) = 0.0
     allocate(visc%Kv_turb(isd:ied,jsd:jed,nz+1)) ; visc%Kv_turb(:,:,:) = 0.0
