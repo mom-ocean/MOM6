91d9fcad21e6946c96b7bff9094f779a6ad1e746
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2015-05-18 21:14:46 -0400

    Updated to CVMix 8c78816c7c "Enhanced diffusivity no longer tied to MatchBoth"
    - Commit CVMix/CVMix-src@8c78816c7c6fb396375859048422bf89408bdf13 introduces
      an enhanced diffusivity by default which can be turned off with
      lenhanced_diff=.false. passed to cvmix_kpp_init().
    - Updated MOM_KPP.F90 to turn off the enhanced diffusivity.
      - Hard coded for now.
    - No answer changes.

--- src/parameterizations/vertical/MOM_KPP.F90
+++ src/parameterizations/vertical/MOM_KPP.F90
@@ -264,6 +264,7 @@ logical function KPP_init(paramFile, G, diag, Time, CS, passive)
                        lEkman=CS%computeEkman,             &
                        lMonOb=CS%computeMoninObukhov,      &
                        MatchTechnique=CS%MatchTechnique,   &
+                       lenhanced_diff=.false.,             &
                        CVmix_kpp_params_user=CS%KPP_params )
 
   ! Register diagnostics
