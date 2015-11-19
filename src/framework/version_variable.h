5bfababb0c3bc5e13a0cb1be25f65b35e390d591
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2015-11-19 14:34:37 -0500

    Changed to len=* for version string
    - @underwoo pointed out that we were used a fixed string length (128)
      for a constant string that was not declared as a parameter. Thx.

--- src/framework/version_variable.h
+++ src/framework/version_variable.h
@@ -1,5 +1,5 @@
 #ifdef _FILE_VERSION
-  character(len=128) :: version = _FILE_VERSION
+  character(len=*), parameter :: version = _FILE_VERSION
 #else
-  character(len=128) :: version = 'unknown'
+  character(len=*), parameter :: version = 'unknown'
 #endif
