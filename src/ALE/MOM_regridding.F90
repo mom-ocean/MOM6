59804c895ec634db67df6749646eb71dd425ff44
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2016-01-19 19:07:45 -0500

    Swap loop nesting in MOM_regridding.F() buildGridRho()
    - i,j loops were nested back-to-front.
    - No answer changes.

--- src/ALE/MOM_regridding.F90
+++ src/ALE/MOM_regridding.F90
@@ -772,8 +772,8 @@ subroutine buildGridRho( G, h, tv, dzInterface, remapCS, CS )
         "Target densities must be set before buildGridRho is called.")
 
   ! Build grid based on target interface densities
-  do i = G%isc-1,G%iec+1
-    do j = G%jsc-1,G%jec+1
+  do j = G%jsc-1,G%jec+1
+    do i = G%isc-1,G%iec+1
 
       ! Copy T and S onto new variables so as to not alter the original values
       ! of T and S (these are remapped at the end of the regridding iterations
@@ -941,8 +941,8 @@ subroutine buildGridRho( G, h, tv, dzInterface, remapCS, CS )
       endif
 #endif
 
-    end do  ! end loop on j
-  end do  ! end loop on i
+    end do  ! end loop on i
+  end do  ! end loop on j
 
 end subroutine buildGridRho
 
