7a6d3d75e8420836183f4651169ee6fb0ffc2d47
Raphael Dussin <raphael.dussin@gmail.com>
2016-04-01 15:51:27 -0400

    RD: temporary fix for initial condition cyclic

--- src/initialization/MOM_tracer_initialization_from_Z.F90
+++ src/initialization/MOM_tracer_initialization_from_Z.F90
@@ -690,7 +690,7 @@ subroutine horiz_interp_and_extrap_tracer(filename, varnam,  conversion, recnum,
 
     if (k == 1) then
       call horiz_interp_new(Interp,x_in,y_in,lon_out(is:ie,js:je),lat_out(is:ie,js:je), &
-               interp_method='bilinear',src_modulo=reentrant_x)
+               interp_method='bilinear',src_modulo=.true.)
     endif
 
     if (debug) then
