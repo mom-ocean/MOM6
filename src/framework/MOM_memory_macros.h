5841cdd830368510f4cda946620427bc462296c3
Robert Hallberg <Robert.Hallberg@noaa.gov>
2016-04-08 11:38:33 -0400

    Added clarifying comments to MOM_memory_macros.h.
      Added comments to MOM_memory_macros.h explaining the proper memory macros to
    use in various circumstances.  Only comments were changed.

--- src/framework/MOM_memory_macros.h
+++ src/framework/MOM_memory_macros.h
@@ -16,6 +16,8 @@
 #  define NIMEM_ (((NIGLOBAL_-1)/NIPROC_)+1+2*NIHALO_)
 #  define NJMEM_ (((NJGLOBAL_-1)/NJPROC_)+1+2*NJHALO_)
 
+! These are the macors that should be used when setting up ALLOCABLE_ or
+! PTR_ (heap) variables.
 #  ifdef SYMMETRIC_MEMORY_
 #    define NIMEMB_   0:NIMEM_
 #    define NJMEMB_   0:NJMEM_
@@ -33,6 +35,8 @@
 #  define C1_         1
 #  define C2_         2
 #  define C3_         3
+! These are the macros that should be used for subroutine arguments
+! or for automatically allocated (stack) variables.
 #  define SZI_(G)     NIMEM_
 #  define SZJ_(G)     NJMEM_
 #  define SZK_(G)     NK_
@@ -50,6 +54,9 @@
 #  define ALLOCABLE_ ,allocatable
 #  define PTR_       ,pointer
 #  define TO_NULL_   =>NULL()
+
+! These are the macors that should be used when setting up ALLOCABLE_ or
+! PTR_ (heap) variables.
 #  define NIMEM_      :
 #  define NJMEM_      :
 #  define NIMEMB_PTR_ :
@@ -69,6 +76,8 @@
 #  define C1_         :
 #  define C2_         :
 #  define C3_         :
+! These are the macros that should be used for subroutine arguments
+! or for automatically allocated (stack) variables.
 #  define SZI_(G)     G%isd:G%ied
 #  define SZJ_(G)     G%jsd:G%jed
 #  define SZK_(G)     G%ke
