3dd705d68d7f0da130fe18bc5686fef9a0ca271a
Niki Zadeh <Niki.Zadeh@noaa.gov>
2016-08-09 10:26:46 -0400

    Adding NEMO as an EOS option
    - The approximate polynomials for density, alpha and beta were provided
      by Fabien Roquet in the form of a code snippet. He gave the reference
      Roquet, F., Madec, G., McDougall, T. J., and Barker, P. M., 2015.
      Accurate polynomial expressions for the density and specific volume of
      seawater using the TEOS-10 standard. Ocean Modelling, 90:29-43.
    
    - The corresponding approximation for compressibility is lacking and
      it is calculated via a TEOS10 function call.
    - The corresponding approximation for specvol deriverties is lacking and
      these are calculated explicitly using the density derivatives.

--- src/equation_of_state/MOM_EOS_TEOS10.F90
+++ src/equation_of_state/MOM_EOS_TEOS10.F90
@@ -21,16 +21,12 @@ module MOM_EOS_TEOS10
 
 !***********************************************************************
 !*  The subroutines in this file implement the equation of state for   *
-!*  sea water using the formulae given by  Wright, 1997, J. Atmos.     *
-!*  Ocean. Tech., 14, 735-740.  Coded by R. Hallberg, 7/00.            *
+!*  sea water using the TEOS10 functions                               *
 !***********************************************************************
 
-use MOM_hor_index, only : hor_index_type
 use gsw_mod_toolbox, only : gsw_rho, gsw_rho_first_derivatives, gsw_specvol_first_derivatives
 
 implicit none ; private
-
-#include <MOM_memory.h>
 
 public calculate_compress_teos10, calculate_density_teos10
 public calculate_density_derivs_teos10, calculate_2_densities_teos10
@@ -41,23 +37,6 @@ interface calculate_density_teos10
   module procedure calculate_density_scalar_teos10, calculate_density_array_teos10
 end interface calculate_density_teos10
 
-!real :: a0, a1, a2, b0, b1, b2, b3, b4, b5, c0, c1, c2, c3, c4, c5
-!    One of the two following blocks of values should be commented out.
-!  Following are the values for the full range formula.
-!
-!real, parameter :: a0 = 7.133718e-4, a1 = 2.724670e-7, a2 = -1.646582e-7
-!real, parameter :: b0 = 5.613770e8,  b1 = 3.600337e6,  b2 = -3.727194e4
-!real, parameter :: b3 = 1.660557e2,  b4 = 6.844158e5,  b5 = -8.389457e3
-!real, parameter :: c0 = 1.609893e5,  c1 = 8.427815e2,  c2 = -6.931554
-!real, parameter :: c3 = 3.869318e-2, c4 = -1.664201e2, c5 = -2.765195
-
-
-! Following are the values for the reduced range formula.
-real, parameter :: a0 = 7.057924e-4, a1 = 3.480336e-7, a2 = -1.112733e-7
-real, parameter :: b0 = 5.790749e8,  b1 = 3.516535e6,  b2 = -4.002714e4
-real, parameter :: b3 = 2.084372e2,  b4 = 5.944068e5,  b5 = -9.643486e3
-real, parameter :: c0 = 1.704853e5,  c1 = 7.904722e2,  c2 = -7.984422
-real, parameter :: c3 = 5.140652e-2, c4 = -2.302158e2, c5 = -3.079464
 
 contains
 
@@ -75,8 +54,7 @@ real,    intent(out) :: rho
 ! *  This subroutine computes the in situ density of sea water (rho in *
 ! *  units of kg/m^3) from salinity (S in psu), potential temperature  *
 ! *  (T in deg C), and pressure in Pa.  It uses the expression from    *
-! *  Wright, 1997, J. Atmos. Ocean. Tech., 14, 735-740.                *
-! *  Coded by R. Hallberg, 7/00                                        *
+! *  TEOS10 website.                                                   *
 ! *====================================================================*
 
   real :: al0, p0, lambda
