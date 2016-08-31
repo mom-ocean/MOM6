d04c76f43d07a6a4454dc03bb7095d488576ca25
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2016-08-31 16:00:26 -0400

    Renamed unitTests to unit_tests
    - In the spirit of abandoning camelCase in favor of snake_case I've
      replaced the use of *unitTests with *_unit_tests in four modules.
    - No answer changes.

--- src/framework/MOM_string_functions.F90
+++ src/framework/MOM_string_functions.F90
@@ -34,7 +34,7 @@ implicit none ; private
 public lowercase, uppercase
 public left_int, left_ints
 public left_real, left_reals
-public stringFunctionsUnitTests
+public string_functions_unit_tests
 public extractWord
 public slasher
 
@@ -245,13 +245,13 @@ function extractWord(string,n)
   if (b<=ns) extractWord = trim(string(b:ns))
 end function extractWord
 
-logical function stringFunctionsUnitTests()
+logical function string_functions_unit_tests()
   ! Should only be called from a single/root thread
   ! Returns True is a test fails, otherwise False
   integer :: i(5) = (/ -1, 1, 3, 3, 0 /)
   real :: r(8) = (/ 0., 1., -2., 1.3, 3.E-11, 3.E-11, 3.E-11, -5.1E12 /)
-  stringFunctionsUnitTests = .false.
-  write(*,*) '===== MOM_string_functions: stringFunctionsUnitTests ====='
+  string_functions_unit_tests = .false.
+  write(*,*) '==== MOM_string_functions: string_functions_unit_tests ==='
   call localTest(left_int(i(1)),'-1')
   call localTest(left_ints(i(:)),'-1, 1, 3, 3, 0')
   call localTest(left_real(r(1)),'0.0')
@@ -269,9 +269,9 @@ logical function stringFunctionsUnitTests()
     character(len=*) :: str1, str2
     write(*,*) '>'//trim(str1)//'<'
     if (trim(str1)/=trim(str2)) write(*,*) 'FAIL:',trim(str1),':',trim(str2)
-    if (trim(str1)/=trim(str2)) stringFunctionsUnitTests=.true.
+    if (trim(str1)/=trim(str2)) string_functions_unit_tests=.true.
   end subroutine localTest
-end function stringFunctionsUnitTests
+end function string_functions_unit_tests
 
 !> Returns a directory name that is terminated with a "/" or "./" if the
 !! argument is an empty string.
