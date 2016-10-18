49b888a5b66ff8a28bb25b5cb102f03cad3581a4
Niki Zadeh <Niki.Zadeh@noaa.gov>
2016-10-18 14:36:08 -0400

    change symbolic link files to copies
    - fremake list_paths tool refuses to discover symbolic link files.
      Make copies of TEOS-10 files instead of using links

--- src/equation_of_state/TEOS10/gsw_mod_kinds.f90
+++ src/equation_of_state/TEOS10/gsw_mod_kinds.f90
@@ -1,1 +1,16 @@
-../../../pkg/GSW-Fortran/modules/gsw_mod_kinds.f90
+!==========================================================================
+module gsw_mod_kinds
+!==========================================================================
+
+implicit none
+
+integer, parameter :: r4 = selected_real_kind(6,30)
+
+integer, parameter :: r8 = selected_real_kind(14,30)
+
+end module
+
+!--------------------------------------------------------------------------
+
+
+
