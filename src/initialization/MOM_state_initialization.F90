959860305d2c841b0146d99a260137721435153f
Kate Hedstrom <kshedstrom@alaska.edu>
2017-01-25 22:20:40 -0900

    GRADIENT OBC works for small problem.
    - Blows up for full scale supercritical.
    - Also cleaned up supercritical user file - can get rid of it once
      allocations happen more centrally.

--- src/initialization/MOM_state_initialization.F90
+++ src/initialization/MOM_state_initialization.F90
@@ -72,7 +72,6 @@ use Rossby_front_2d_initialization, only : Rossby_front_initialize_temperature_s
 use Rossby_front_2d_initialization, only : Rossby_front_initialize_velocity
 use SCM_idealized_hurricane, only : SCM_idealized_hurricane_TS_init
 use SCM_CVmix_tests, only: SCM_CVmix_tests_TS_init
-use supercritical_initialization, only : supercritical_initialize_velocity
 use supercritical_initialization, only : supercritical_set_OBC_data
 use soliton_initialization, only : soliton_initialize_velocity
 use soliton_initialization, only : soliton_initialize_thickness
@@ -342,7 +341,6 @@ subroutine MOM_initialize_state(u, v, h, tv, Time, G, GV, PF, dirs, &
        case ("phillips"); call Phillips_initialize_velocity(u, v, G, GV, PF)
        case ("rossby_front"); call Rossby_front_initialize_velocity(u, v, h, G, GV, PF)
        case ("soliton"); call soliton_initialize_velocity(u, v, h, G)
-       case ("supercritical"); call supercritical_initialize_velocity(u, v, h, G, PF)
        case ("USER"); call user_initialize_velocity(u, v, G, PF)
        case default ; call MOM_error(FATAL,  "MOM_initialize_state: "//&
             "Unrecognized velocity configuration "//trim(config))
