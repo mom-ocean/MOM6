61f9de1c1d21ca7cb01956f8057af33fff9978b3
Kate Hedstrom <kshedstrom@alaska.edu>
2017-02-06 10:05:52 -0900

    Last fix to get barotropic radiation sort of running.
    - Also update to MOM_open_boundary for intent(inout) for gnu version 4.7.3.

--- src/core/MOM_barotropic.F90
+++ src/core/MOM_barotropic.F90
@@ -2418,7 +2418,7 @@ subroutine apply_velocity_OBCs(OBC, ubt, vbt, uhbt, vhbt, ubt_trans, vbt_trans, 
           endif
           ubt(I,j) = ((cff*ubt_old(I,j) + Cx*ubt(I-1,j)) - &
               (max(Cy,0.0)*grad(I,J-1) + min(Cy,0.0)*grad(I,J))) / (cff + Cx)
-          vel_trans = (1.0-bebt)*vel_prev + bebt*ubt(I,j)
+          vel_trans = ubt(I,j)
         elseif (OBC%segment(OBC%OBC_segment_u(I,j))%gradient) then
           ubt(I,j) = ubt(I-1,j)
           vel_trans = ubt(I,j)
@@ -2468,34 +2468,6 @@ subroutine apply_velocity_OBCs(OBC, ubt, vbt, uhbt, vhbt, ubt_trans, vbt_trans, 
           ubt(I,j) = ubt(I+1,j)
           vel_trans = ubt(I,j)
         endif
-!!!!! elseif (OBC%segment(OBC%OBC_segment_u(I,j))%direction == OBC_DIRECTION_N) then
-!!!!!   if (OBC%segment(OBC%OBC_segment_u(I,j))%legacy) then
-!!!!!     if ((vbt(i,J-1)+vbt(i+1,J-1)) > 0.0) then
-!!!!!       ubt(I,j) = 2.0*ubt(I,j-1)-ubt(I,j-2)
-!!!!!     else
-!!!!!       ubt(I,j) = BT_OBC%ubt_outer(I,j)
-!!!!!     endif
-!!!!!   else
-!!!!!     Cy = dtbt * G%dyCu(I,j) * sqrt(OBC%g_Earth * 0.5 * &
-!!!!!            (G%bathyT(i,j) + eta(i,j) + G%bathyT(i+1,j) + eta(i+1,j)))
-!!!!!     cff = 1.0 / (1.0 + Cy)
-!!!!!     ubt(I,j) = cff * (ubt(I,j) + Cy * ubt(I,j-1))
-!!!!!   endif
-!!!!!   vel_trans = ubt(I,j)
-!!!!! elseif (OBC%segment(OBC%OBC_segment_u(I,j))%direction == OBC_DIRECTION_S) then
-!!!!!   if (OBC%segment(OBC%OBC_segment_u(I,j))%legacy) then
-!!!!!     if ((vbt(i,J)+vbt(i+1,J)) < 0.0) then
-!!!!!       ubt(I,j) = 2.0*ubt(I,j+1)-ubt(I,j+2)
-!!!!!     else
-!!!!!       ubt(I,j) = BT_OBC%ubt_outer(I,j)
-!!!!!     endif
-!!!!!   else
-!!!!!     Cy = dtbt * G%dyCu(I,j) * sqrt(OBC%g_Earth * 0.5 * &
-!!!!!            (G%bathyT(i,j) + eta(i,j) + G%bathyT(i+1,j) + eta(i+1,j)))
-!!!!!     cff = 1.0 / (1.0 + Cy)
-!!!!!     ubt(I,j) = cff * (ubt(I,j) + Cy * ubt(I,j+1))
-!!!!!   endif
-!!!!!   vel_trans = ubt(I,j)
       endif
 
       if (.not. OBC%segment(OBC%OBC_segment_u(I,j))%specified) then
@@ -2606,42 +2578,6 @@ subroutine apply_velocity_OBCs(OBC, ubt, vbt, uhbt, vhbt, ubt_trans, vbt_trans, 
           vbt(i,J) = vbt(i,J+1)
           vel_trans = vbt(i,J)
         endif
-!!!!! elseif (OBC%segment(OBC%OBC_segment_v(i,J))%direction == OBC_DIRECTION_E) then
-!!!!!   if (OBC%segment(OBC%OBC_segment_v(i,J))%legacy) then
-!!!!!     if ((ubt(I-1,j)+ubt(I-1,j+1)) > 0.0) then
-!!!!!       vbt(i,J) = 2.0*vbt(i-1,J)-vbt(i-2,J)
-!!!!!     else
-!!!!!       vbt(i,J) = BT_OBC%vbt_outer(i,J)
-!!!!!     endif
-!!!!!   else
-!!!!!     Cx = dtbt * G%dxCv(I,j) * sqrt(OBC%g_Earth * 0.5 * &
-!!!!!            (G%bathyT(i,j) + eta(i,j) + G%bathyT(i,j+1) + eta(i,j+1)))
-!!!!!     cff = 1.0 / (1.0 + Cx)
-!!!!!     vbt(i,J) = cff * (vbt(i,J) + Cx * vbt(i+1,J))
-!!!!!   endif
-!!!!!   vel_trans = vbt(i,J)
-!!!!!!!!!!!!!!!!!!! CLAMPED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!       cfl = dtbt * BT_OBC%Cg_v(i,J) * G%IdyCv(i,J)           !
-!       vbt(i,J) = (vbt(i-1,J) + CFL*vbt(i,J)) / (1.0 + CFL)  !
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!! elseif (OBC%segment(OBC%OBC_segment_v(i,J))%direction == OBC_DIRECTION_W) then
-!!!!!   if (OBC%segment(OBC%OBC_segment_v(i,J))%legacy) then
-!!!!!     if ((ubt(I,j)+ubt(I,j+1)) < 0.0) then
-!!!!!       vbt(i,J) = 2.0*vbt(i+1,J)-vbt(i+2,J)
-!!!!!     else
-!!!!!       vbt(i,J) = BT_OBC%vbt_outer(i,J)
-!!!!!     endif
-!!!!!   else
-!!!!!     Cx = dtbt * G%dxCv(I,j) * sqrt(OBC%g_Earth * 0.5 * &
-!!!!!            (G%bathyT(i,j) + eta(i,j) + G%bathyT(i,j+1) + eta(i,j+1)))
-!!!!!     cff = 1.0 / (1.0 + Cx)
-!!!!!     vbt(i,J) = cff * (vbt(i,J) + Cx * vbt(i-1,J))
-!!!!!   endif
-!!!!!   vel_trans = vbt(i,J)
-!!!!!!!!!!!!!!!!!! CLAMPED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!       cfl = dtbt * BT_OBC%Cg_v(i,J) * G%IdyCv(i,J)           !
-!       vbt(i,J) = (vbt(i-1,J) + CFL*vbt(i,J)) / (1.0 + CFL)  !
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       endif
 
       if (.not. OBC%segment(OBC%OBC_segment_v(i,J))%specified) then
