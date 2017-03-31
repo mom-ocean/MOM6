849a89a31db68749be58a9c30f87808c0af6ccd9
Kate Hedstrom <kshedstrom@alaska.edu>
2017-03-31 11:29:17 -0800

    Kelvin wave cleanup (but not running).

--- src/user/Kelvin_initialization.F90
+++ src/user/Kelvin_initialization.F90
@@ -81,9 +81,10 @@ subroutine Kelvin_set_OBC_data(OBC, G, h, Time)
       OBC%eta_outer_u(I,j) = val2 * cos(omega * time_sec)
       OBC%ubt_outer(I,j) = val1 * cff / (0.5 *              &
                (G%bathyT(i+1,j) + G%bathyT(i,j))) * val2
-      segment%eta(I,j) = val2 * cos(omega * time_sec)
-      segment%normal_vel_bt(I,j) = val1 * cff / (0.5 *      &
-               (G%bathyT(i+1,j) + G%bathyT(i,j))) * val2
+! New way, not yet
+!     segment%eta(I,j) = val2 * cos(omega * time_sec)
+!     segment%normal_vel_bt(I,j) = val1 * cff / (0.5 *      &
+!              (G%bathyT(i+1,j) + G%bathyT(i,j))) * val2
     enddo ; enddo
   enddo
 
