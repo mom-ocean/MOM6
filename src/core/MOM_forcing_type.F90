a6a2b35cea5d9468ea0a0658067bbd3533ae0971
Robert Hallberg <Robert.Hallberg@noaa.gov>
2017-04-27 15:36:53 -0400

    Corrected uvchksum call arguments for tau[xy]
      Corrected the arguments for uvchksum calls for fluxes%tau[xy], so these calls now properly reflect the updated portions of these
    arrays.  All answers are bitwise identical.

--- src/core/MOM_forcing_type.F90
+++ src/core/MOM_forcing_type.F90
@@ -815,7 +815,8 @@ subroutine MOM_forcing_chksum(mesg, fluxes, G, haloshift)
   ! counts, there must be no redundant points, so all variables use is..ie
   ! and js...je as their extent.
   if (associated(fluxes%taux) .and. associated(fluxes%tauy)) &
-    call uvchksum(mesg//" fluxes%tau[xy]", fluxes%taux, fluxes%tauy, G%HI, haloshift=1)
+    call uvchksum(mesg//" fluxes%tau[xy]", fluxes%taux, fluxes%tauy, G%HI, &
+                  haloshift=hshift, symmetric=.true.)
   if (associated(fluxes%ustar)) &
     call hchksum(fluxes%ustar, mesg//" fluxes%ustar",G%HI,haloshift=hshift)
   if (associated(fluxes%buoy)) &
