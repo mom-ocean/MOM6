14c17f5db746865a8e6a0dd0fcb1b4a49fbf152d
Robert Hallberg <Robert.Hallberg@noaa.gov>
2017-08-10 14:44:11 -0400

    +Allocate zero-sized values arrays in CT_spawn
      When the various coupler_type_spawn arrays are called with starting indicies
    that exceed the ending indicies, allocate the values arrays anyway, but as zero
    sized arrays, following the previous behavior with these types from the old FMS
    version of these arrays.  This does not change answers, but this change can
    prevent the warsaw and older versions of the coupler from hanging in certain
    coupled runs, and it keeps these copies in synch with the intended versions for
    the next release of FMS.

--- config_src/solo_driver/coupler_types.F90
+++ config_src/solo_driver/coupler_types.F90
@@ -639,10 +639,9 @@ subroutine CT_spawn_1d_2d(var_in, var, idim, jdim, suffix, as_needed)
           write (error_msg, *) trim(error_header), ' var%bc(', n, ')%field(', m, ')%values already associated'
           call mpp_error(FATAL, trim(error_msg))
         endif
-        if ((var%isd<=var%ied) .and. (var%jsd<=var%jed)) then
-          allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed) )
-          var%bc(n)%field(m)%values(:,:) = 0.0
-        endif
+        ! Note that this may be allocating a zero-sized array, which is legal in Fortran.
+        allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed) )
+        var%bc(n)%field(m)%values(:,:) = 0.0
       enddo
     enddo
 
@@ -746,10 +745,9 @@ subroutine CT_spawn_1d_3d(var_in, var, idim, jdim, kdim, suffix, as_needed)
           write (error_msg, *) trim(error_header), ' var%bc(', n, ')%field(', m, ')%values already associated'
           call mpp_error(FATAL, trim(error_msg))
         endif
-        if ((var%isd<=var%ied) .and. (var%jsd<=var%jed) .and. (var%ks<=var%ke)) then
-          allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed,var%ks:var%ke) )
-          var%bc(n)%field(m)%values(:,:,:) = 0.0
-        endif
+        ! Note that this may be allocating a zero-sized array, which is legal in Fortran.
+        allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed,var%ks:var%ke) )
+        var%bc(n)%field(m)%values(:,:,:) = 0.0
       enddo
     enddo
 
@@ -845,10 +843,9 @@ subroutine CT_spawn_2d_2d(var_in, var, idim, jdim, suffix, as_needed)
           write (error_msg, *) trim(error_header), ' var%bc(', n, ')%field(', m, ')%values already associated'
           call mpp_error(FATAL, trim(error_msg))
         endif
-        if ((var%isd<=var%ied) .and. (var%jsd<=var%jed)) then
-          allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed) )
-          var%bc(n)%field(m)%values(:,:) = 0.0
-        endif
+        ! Note that this may be allocating a zero-sized array, which is legal in Fortran.
+        allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed) )
+        var%bc(n)%field(m)%values(:,:) = 0.0
       enddo
     enddo
 
@@ -952,10 +949,9 @@ subroutine CT_spawn_2d_3d(var_in, var, idim, jdim, kdim, suffix, as_needed)
           write (error_msg, *) trim(error_header), ' var%bc(', n, ')%field(', m, ')%values already associated'
           call mpp_error(FATAL, trim(error_msg))
         endif
-        if ((var%isd<=var%ied) .and. (var%jsd<=var%jed) .and. (var%ks<=var%ke)) then
-          allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed,var%ks:var%ke) )
-          var%bc(n)%field(m)%values(:,:,:) = 0.0
-        endif
+        ! Note that this may be allocating a zero-sized array, which is legal in Fortran.
+        allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed,var%ks:var%ke) )
+        var%bc(n)%field(m)%values(:,:,:) = 0.0
       enddo
     enddo
 
@@ -1051,10 +1047,9 @@ subroutine CT_spawn_3d_2d(var_in, var, idim, jdim, suffix, as_needed)
           write (error_msg, *) trim(error_header), ' var%bc(', n, ')%field(', m, ')%values already associated'
           call mpp_error(FATAL, trim(error_msg))
         endif
-        if ((var%isd<=var%ied) .and. (var%jsd<=var%jed)) then
-          allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed) )
-          var%bc(n)%field(m)%values(:,:) = 0.0
-        endif
+        ! Note that this may be allocating a zero-sized array, which is legal in Fortran.
+        allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed) )
+        var%bc(n)%field(m)%values(:,:) = 0.0
       enddo
     enddo
 
@@ -1157,10 +1152,10 @@ subroutine CT_spawn_3d_3d(var_in, var, idim, jdim, kdim, suffix, as_needed)
           write (error_msg, *) trim(error_header), ' var%bc(', n, ')%field(', m, ')%values already associated'
           call mpp_error(FATAL, trim(error_msg))
         endif
-        if ((var%isd<=var%ied) .and. (var%jsd<=var%jed) .and. (var%ks<=var%ke)) then
-          allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed,var%ks:var%ke) )
-          var%bc(n)%field(m)%values(:,:,:) = 0.0
-        endif
+
+        ! Note that this may be allocating a zero-sized array, which is legal in Fortran.
+        allocate ( var%bc(n)%field(m)%values(var%isd:var%ied,var%jsd:var%jed,var%ks:var%ke) )
+        var%bc(n)%field(m)%values(:,:,:) = 0.0
       enddo
     enddo
 
