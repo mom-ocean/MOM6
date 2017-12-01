8e4348caf5dbda06566b950e1f9561aa3c7f040e
Kate Hedstrom <kshedstrom@alaska.edu>
2017-11-30 15:07:32 -0900

    Fix up dyed_channel for both tracers and time-dependence
    - Needs OBC tracer registry
    - Needs boundary update call

--- src/user/dyed_channel_initialization.F90
+++ src/user/dyed_channel_initialization.F90
@@ -10,6 +10,8 @@ use MOM_grid,            only : ocean_grid_type
 use MOM_io,              only : vardesc, var_desc
 use MOM_open_boundary,   only : ocean_OBC_type, OBC_NONE, OBC_SIMPLE
 use MOM_open_boundary,   only : OBC_segment_type, register_segment_tracer
+use MOM_open_boundary,   only : OBC_registry_type, register_OBC
+use MOM_time_manager,    only : time_type, set_time, time_type_to_real
 use MOM_tracer_registry, only : tracer_registry_type, add_tracer_OBC_values
 use MOM_variables,       only : thermo_var_ptrs
 use MOM_verticalGrid,    only : verticalGrid_type
@@ -18,14 +20,59 @@ implicit none ; private
 
 #include <MOM_memory.h>
 
-public dyed_channel_set_OBC_data
+public dyed_channel_set_OBC_tracer_data, dyed_channel_OBC_end
+public register_dyed_channel_OBC, dyed_channel_update_flow
+
+!> Control structure for tidal bay open boundaries.
+type, public :: dyed_channel_OBC_CS ; private
+  real :: zonal_flow = 8.57         !< Maximum inflow
+  real :: frequency  = 0.0         !< Inflow frequency
+end type dyed_channel_OBC_CS
 
 integer :: ntr = 0
 
 contains
 
+!> Add dyed channel to OBC registry.
+function register_dyed_channel_OBC(param_file, CS, OBC_Reg)
+  type(param_file_type),     intent(in) :: param_file !< parameter file.
+  type(dyed_channel_OBC_CS), pointer    :: CS         !< tidal bay control structure.
+  type(OBC_registry_type),   pointer    :: OBC_Reg    !< OBC registry.
+  logical                               :: register_dyed_channel_OBC
+  character(len=32)  :: casename = "dyed channel"     !< This case's name.
+  character(len=40)  :: mdl = "register_dyed_channel_OBC" ! This subroutine's name.
+
+  if (associated(CS)) then
+    call MOM_error(WARNING, "register_dyed_channel_OBC called with an "// &
+                            "associated control structure.")
+    return
+  endif
+  allocate(CS)
+
+  call get_param(param_file, mdl, "SUPERCRITICAL_ZONAL_FLOW", CS%zonal_flow, &
+                 "Constant zonal flow imposed at upstream open boundary.", &
+                 units="m/s", default=8.57)
+  call get_param(param_file, mdl, "CHANNEL_FLOW_FREQUENCY", CS%frequency, &
+                 "Frequency of oscillating zonal flow.", &
+                 units="s-1", default=0.0)
+
+  ! Register the open boundaries.
+  call register_OBC(casename, param_file, OBC_Reg)
+  register_dyed_channel_OBC = .true.
+
+end function register_dyed_channel_OBC
+
+!> Clean up the dyed_channel OBC from registry.
+subroutine dyed_channel_OBC_end(CS)
+  type(dyed_channel_OBC_CS), pointer :: CS    !< tidal bay control structure.
+
+  if (associated(CS)) then
+    deallocate(CS)
+  endif
+end subroutine dyed_channel_OBC_end
+
 !> This subroutine sets the dye and flow properties at open boundary conditions.
-subroutine dyed_channel_set_OBC_data(OBC, G, GV, param_file, tr_Reg)
+subroutine dyed_channel_set_OBC_tracer_data(OBC, G, GV, param_file, tr_Reg)
   type(ocean_OBC_type),       pointer    :: OBC !< This open boundary condition type specifies
                                                 !! whether, where, and what open boundary
                                                 !! conditions are used.
@@ -36,56 +83,16 @@ subroutine dyed_channel_set_OBC_data(OBC, G, GV, param_file, tr_Reg)
   type(tracer_registry_type), pointer    :: tr_Reg !< Tracer registry.
 
 ! Local variables
-  character(len=40)  :: mdl = "dyed_channel_set_OBC_data" ! This subroutine's name.
+  character(len=40)  :: mdl = "dyed_channel_set_OBC_tracer_data" ! This subroutine's name.
   character(len=80)  :: name, longname
-  real :: zonal_flow
-  integer :: i, j, k, l, itt, isd, ied, jsd, jed, m, n, nz
+  integer :: i, j, k, l, itt, isd, ied, jsd, jed, m, n
   integer :: IsdB, IedB, JsdB, JedB
   real :: dye
   type(OBC_segment_type), pointer :: segment
   type(vardesc), allocatable, dimension(:) :: tr_desc
 
-  nz = G%ke
-  isd = G%isd ; ied = G%ied ; jsd = G%jsd ; jed = G%jed
-  IsdB = G%IsdB ; IedB = G%IedB ; JsdB = G%JsdB ; JedB = G%JedB
-
   if (.not.associated(OBC)) call MOM_error(FATAL, 'dyed_channel_initialization.F90: '// &
         'dyed_channel_set_OBC_data() was called but OBC type was not initialized!')
-
-  call get_param(param_file, mdl, "SUPERCRITICAL_ZONAL_FLOW", zonal_flow, &
-                 "Constant zonal flow imposed at upstream open boundary.", &
-                 units="m/s", default=8.57)
-
-  do l=1, OBC%number_of_segments
-    segment => OBC%segment(l)
-    if (.not. segment%on_pe) cycle
-    if (segment%gradient) cycle
-    if (segment%oblique .and. .not. segment%nudged .and. .not. segment%Flather) cycle
-
-    if (segment%is_E_or_W) then
-      jsd = segment%HI%jsd ; jed = segment%HI%jed
-      IsdB = segment%HI%IsdB ; IedB = segment%HI%IedB
-      do k=1,G%ke
-        do j=jsd,jed ; do I=IsdB,IedB
-          if (segment%specified .or. segment%nudged) then
-            segment%normal_vel(I,j,k) = zonal_flow
-          endif
-          if (segment%specified) then
-            segment%normal_trans(I,j,k) = zonal_flow * G%dyCu(I,j)
-          endif
-        enddo ; enddo
-      enddo
-      do j=jsd,jed ; do I=IsdB,IedB
-        segment%normal_vel_bt(I,j) = zonal_flow
-      enddo ; enddo
-    else
-      isd = segment%HI%isd ; ied = segment%HI%ied
-      JsdB = segment%HI%JsdB ; JedB = segment%HI%JedB
-      do J=JsdB,JedB ; do i=isd,ied
-        segment%normal_vel_bt(i,J) = 0.0
-      enddo ; enddo
-    endif
-  enddo
 
   call get_param(param_file, mdl, "NUM_DYE_TRACERS", ntr, &
                  "The number of dye tracers in this run. Each tracer \n"//&
@@ -101,8 +108,8 @@ subroutine dyed_channel_set_OBC_data(OBC, G, GV, param_file, tr_Reg)
 ! ! Set the inflow values of the dyes, one per segment.
 ! ! We know the order: north, south, east, west
   do m=1,ntr
-    write(name,'("dye_",I1.1)') m
-    write(longname,'("Concentration of dyed_obc Tracer ",I1.1, " on segment ",I1.1)') m, m
+    write(name,'("dye_",I2.2)') m
+    write(longname,'("Concentration of dyed_obc Tracer ",I2.2, " on segment ",I2.2)') m, m
     tr_desc(m) = var_desc(name, units="kg kg-1", longname=longname, caller=mdl)
 
     do n=1,OBC%number_of_segments
@@ -117,7 +124,68 @@ subroutine dyed_channel_set_OBC_data(OBC, G, GV, param_file, tr_Reg)
   enddo
   deallocate(tr_desc)
 
-end subroutine dyed_channel_set_OBC_data
+end subroutine dyed_channel_set_OBC_tracer_data
+
+!> This subroutine updates the long-channel flow
+subroutine dyed_channel_update_flow(OBC, CS, G, Time)
+  type(ocean_OBC_type),       pointer    :: OBC !< This open boundary condition type specifies
+                                                !! whether, where, and what open boundary
+                                                !! conditions are used.
+  type(dyed_channel_OBC_CS),  pointer    :: CS  !< tidal bay control structure.
+  type(ocean_grid_type),      intent(in) :: G   !< The ocean's grid structure.
+  type(time_type),            intent(in) :: Time !< model time.
+
+! Local variables
+  character(len=40)  :: mdl = "dyed_channel_update_flow" ! This subroutine's name.
+  character(len=80)  :: name, longname
+  real :: flow, time_sec, PI
+  integer :: i, j, k, l, itt, isd, ied, jsd, jed, m, n
+  integer :: IsdB, IedB, JsdB, JedB
+  type(OBC_segment_type), pointer :: segment
+
+  if (.not.associated(OBC)) call MOM_error(FATAL, 'dyed_channel_initialization.F90: '// &
+        'dyed_channel_update_flow() was called but OBC type was not initialized!')
+
+  time_sec = time_type_to_real(Time)
+  PI = 4.0*atan(1.0)
+
+  do l=1, OBC%number_of_segments
+    segment => OBC%segment(l)
+    if (.not. segment%on_pe) cycle
+    if (segment%gradient) cycle
+    if (segment%oblique .and. .not. segment%nudged .and. .not. segment%Flather) cycle
+
+    if (segment%is_E_or_W) then
+      jsd = segment%HI%jsd ; jed = segment%HI%jed
+      IsdB = segment%HI%IsdB ; IedB = segment%HI%IedB
+      if (CS%frequency == 0.0) then
+        flow = CS%zonal_flow
+      else
+        flow = CS%zonal_flow * cos(2 * PI * CS%frequency * time_sec)
+      endif
+      do k=1,G%ke
+        do j=jsd,jed ; do I=IsdB,IedB
+          if (segment%specified .or. segment%nudged) then
+            segment%normal_vel(I,j,k) = flow
+          endif
+          if (segment%specified) then
+            segment%normal_trans(I,j,k) = flow * G%dyCu(I,j)
+          endif
+        enddo ; enddo
+      enddo
+      do j=jsd,jed ; do I=IsdB,IedB
+        segment%normal_vel_bt(I,j) = flow
+      enddo ; enddo
+    else
+      isd = segment%HI%isd ; ied = segment%HI%ied
+      JsdB = segment%HI%JsdB ; JedB = segment%HI%JedB
+      do J=JsdB,JedB ; do i=isd,ied
+        segment%normal_vel_bt(i,J) = 0.0
+      enddo ; enddo
+    endif
+  enddo
+
+end subroutine dyed_channel_update_flow
 
 !> \namespace dyed_channel_initialization
 !! Setting dyes, one for painting the inflow on each side.
