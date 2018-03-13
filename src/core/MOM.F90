a209b7a8a02a61201f3c505ae48e904c2f26d2a5
Gustavo Marques <gmarques@ucar.edu>
2018-03-13 10:36:59 -0600

    Added a call to diabatic_driver_end

--- src/core/MOM.F90
+++ src/core/MOM.F90
@@ -2900,6 +2900,8 @@ subroutine MOM_end(CS)
   call tracer_registry_end(CS%tracer_Reg)
   call tracer_flow_control_end(CS%tracer_flow_CSp)
 
+  if (associated(CS%diabatic_CSp)) call diabatic_driver_end(CS%diabatic_CSp)
+
   if (CS%offline_tracer_mode) call offline_transport_end(CS%offline_CSp)
 
   DEALLOC_(CS%uhtr) ; DEALLOC_(CS%vhtr)
