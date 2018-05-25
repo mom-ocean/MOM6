939dcf761d70680e71ce934e8aa86a8805e86918
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2018-05-25 13:50:06 -0400

    Call diabatic_driver_end()
    - The call to diabatic_driver_end() was commented out.

--- src/core/MOM.F90
+++ src/core/MOM.F90
@@ -2991,8 +2991,7 @@ subroutine MOM_end(CS)
   call tracer_registry_end(CS%tracer_Reg)
   call tracer_flow_control_end(CS%tracer_flow_CSp)
 
-  ! GMM, the following is commented because it fails on Travis.
-  !if (associated(CS%diabatic_CSp)) call diabatic_driver_end(CS%diabatic_CSp)
+  call diabatic_driver_end(CS%diabatic_CSp)
 
   if (CS%offline_tracer_mode) call offline_transport_end(CS%offline_CSp)
 
