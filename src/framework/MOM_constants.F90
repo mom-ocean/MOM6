5f5f8147b84e6ea2d683d870648fe75120061471
Robert Hallberg <Robert.Hallberg@noaa.gov>
2018-07-06 18:28:57 -0400

    dOxyGenized a parameter in MOM_constants.F90

--- src/framework/MOM_constants.F90
+++ src/framework/MOM_constants.F90
@@ -7,6 +7,7 @@ use constants_mod, only : HLV, HLF
 
 implicit none ; private
 
+!> The constant offset for converting temperatures in Kelvin to Celsius
 real, public, parameter :: CELSIUS_KELVIN_OFFSET = 273.15
 public :: HLV, HLF
 
