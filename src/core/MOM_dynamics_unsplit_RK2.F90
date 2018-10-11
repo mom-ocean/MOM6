1e3be0f3b25dd1b50f50ee89c77302095a204bbd
Robert Hallberg <Robert.Hallberg@noaa.gov>
2018-10-11 12:13:49 -0400

    Fixed rescaling of eta_av unsplit stepping code
      Corrected the dimensional rescaling of eta_av in step_MOM_dyn_unsplit and
    step_MOM_dyn_unsplit_RK2 to go to H.  This only occurs with Boussinesq code, and
    the answers do not change if H_to_m is 1, as is often the case, and only
    diagnostics are impacted in ocean only cases.  This code appears not to be
    adequately tested with the MOM6_examples test suite, which was bitwise identical
    with this change.

--- src/core/MOM_dynamics_unsplit_RK2.F90
+++ src/core/MOM_dynamics_unsplit_RK2.F90
@@ -221,7 +221,7 @@ subroutine step_MOM_dyn_unsplit_RK2(u_in, v_in, h_in, tv, visc, Time_local, dt, 
                                                               !! or mass transport since the last
                                                               !! tracer advection, in m3 or kg.
   real, dimension(SZI_(G),SZJ_(G)),  intent(out)   :: eta_av  !< The time-mean free surface height
-                                                              !! or column mass, in m or kg m-2.
+                                                              !! or column mass, in H (m or kg m-2).
   type(MOM_dyn_unsplit_RK2_CS),      pointer       :: CS      !< The control structure set up by
                                                               !! initialize_dyn_unsplit_RK2.
   type(VarMix_CS),                   pointer       :: VarMix  !< A pointer to a structure with
@@ -431,7 +431,7 @@ subroutine step_MOM_dyn_unsplit_RK2(u_in, v_in, h_in, tv, visc, Time_local, dt, 
   endif
 
   if (GV%Boussinesq) then
-    do j=js,je ; do i=is,ie ; eta_av(i,j) = -G%Zd_to_m*G%bathyT(i,j) ; enddo ; enddo
+    do j=js,je ; do i=is,ie ; eta_av(i,j) = -GV%Z_to_H*G%bathyT(i,j) ; enddo ; enddo
   else
     do j=js,je ; do i=is,ie ; eta_av(i,j) = 0.0 ; enddo ; enddo
   endif
