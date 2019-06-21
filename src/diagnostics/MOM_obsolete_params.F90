062e3b0d0045817993e21f7b1357e252d4d75198
Gustavo Marques <gmarques@ucar.edu>
2019-06-21 13:49:32 -0600

    Obsolete parameter MEKE_VISCOSITY_COEFF

--- src/diagnostics/MOM_obsolete_params.F90
+++ src/diagnostics/MOM_obsolete_params.F90
@@ -69,6 +69,9 @@ subroutine find_obsolete_params(param_file)
        hint="Instead use OBC_SEGMENT_XXX_DATA.")
   call obsolete_char(param_file, "EXTEND_OBC_SEGMENTS", &
        hint="This option is no longer needed, nor supported.")
+  call obsolete_char(param_file, "MEKE_VISCOSITY_COEFF", &
+       hint="This option has been replaced by MEKE_VISCOSITY_COEFF_KU and" &
+            " MEKE_VISCOSITY_COEFF_AU. Please set these parameters instead.")
   nseg = 0
   call read_param(param_file, "OBC_NUMBER_OF_SEGMENTS", nseg)
   do l_seg = 1,nseg
