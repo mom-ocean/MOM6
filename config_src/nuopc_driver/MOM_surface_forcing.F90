97c2395a58af4f5ad716bb52742fa5d64a07c659
Gustavo Marques <gmarques@ucar.edu>
2019-06-23 17:42:58 -0600

    Commenting out salt_flux adjustment in net_FW in NUOPC cap

--- config_src/nuopc_driver/MOM_surface_forcing.F90
+++ config_src/nuopc_driver/MOM_surface_forcing.F90
@@ -552,9 +552,10 @@ subroutine convert_IOB_to_fluxes(IOB, fluxes, index_bounds, Time, G, US, CS, &
       !   To do this correctly we will need a sea-ice melt field added to IOB. -AJA
       ! GMM: as stated above, the following is wrong. CIME deals with volume/mass and
       ! heat from sea ice/snow via seaice_melt and seaice_melt_heat, respectively.
-      if (associated(IOB%salt_flux) .and. (CS%ice_salt_concentration>0.0)) &
-        net_FW(i,j) = net_FW(i,j) + sign_for_net_FW_bug * G%areaT(i,j) * &
-                     (IOB%salt_flux(i-i0,j-j0) / CS%ice_salt_concentration)
+      !if (associated(IOB%salt_flux) .and. (CS%ice_salt_concentration>0.0)) &
+      !  net_FW(i,j) = net_FW(i,j) + sign_for_net_FW_bug * G%areaT(i,j) * &
+      !               (IOB%salt_flux(i-i0,j-j0) / CS%ice_salt_concentration)
+
       net_FW2(i,j) = net_FW(i,j) / G%areaT(i,j)
     enddo ; enddo
 
