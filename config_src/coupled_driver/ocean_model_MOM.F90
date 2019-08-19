e4404a6e9b13bd3b8e827fcd8ee24c3fe9fde867
wrongkindofdoctor <>
2019-08-19 16:19:51 -0400

    added call to mpp_define_io_domain in initialize_ocean_public_type to facilitate use of new open_file, which requires that these values be associated

--- config_src/coupled_driver/ocean_model_MOM.F90
+++ config_src/coupled_driver/ocean_model_MOM.F90
@@ -54,6 +54,7 @@ use coupler_types_mod, only : coupler_type_initialized, coupler_type_copy_data
 use coupler_types_mod, only : coupler_type_set_diags, coupler_type_send_data
 use mpp_domains_mod, only : domain2d, mpp_get_layout, mpp_get_global_domain
 use mpp_domains_mod, only : mpp_define_domains, mpp_get_compute_domain, mpp_get_data_domain
+use mpp_domains_mod, only : mpp_define_io_domain
 use atmos_ocean_fluxes_mod, only : aof_set_coupler_flux
 use fms_mod, only : stdout
 use mpp_mod, only : mpp_chksum
@@ -787,6 +788,7 @@ subroutine initialize_ocean_public_type(input_domain, Ocean_sfc, diag, maskmap, 
      call mpp_define_domains((/1,xsz,1,ysz/),layout,Ocean_sfc%Domain)
   endif
   call mpp_get_compute_domain(Ocean_sfc%Domain, isc, iec, jsc, jec)
+  call mpp_define_io_domain(Ocean_sfc%Domain, Ocean_sfc%Domain%io_layout)
 
   allocate ( Ocean_sfc%t_surf (isc:iec,jsc:jec), &
              Ocean_sfc%s_surf (isc:iec,jsc:jec), &
