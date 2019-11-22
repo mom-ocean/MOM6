0b921b0be4202031f2f8e62d606e3a64403254a4
Matthew Harrison <Matthew.Harrison@noaa.gov>
2019-11-22 08:57:50 -0500

    added constants from fms for use in oda routines

--- src/framework/MOM_constants.F90
+++ src/framework/MOM_constants.F90
@@ -4,11 +4,15 @@ module MOM_constants
 ! This file is part of MOM6. See LICENSE.md for the license.
 
 use constants_mod, only : HLV, HLF
+use constants_mod, only : radius, seconds_per_hour, seconds_per_day
 
 implicit none ; private
 
 !> The constant offset for converting temperatures in Kelvin to Celsius
 real, public, parameter :: CELSIUS_KELVIN_OFFSET = 273.15
 public :: HLV, HLF
+public :: radius
+public :: seconds_per_hour
+public :: seconds_per_day
 
 end module MOM_constants
