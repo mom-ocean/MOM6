1ee0592940c282461e5820bbc73345ce44f9ea10
Matthew Harrison <Matthew.Harrison@noaa.gov>
2020-02-20 12:15:39 -0500

    Removed midas_vertmap module and relocated associated subroutines related to non-ALE initilization into MOM_tracer_Z_init.

--- src/tracer/MOM_tracer_Z_init.F90
+++ src/tracer/MOM_tracer_Z_init.F90
@@ -7,6 +7,7 @@ use MOM_error_handler, only : MOM_error, FATAL, WARNING, MOM_mesg, is_root_pe
 ! use MOM_file_parser, only : get_param, log_version, param_file_type
 use MOM_grid, only : ocean_grid_type
 use MOM_io, only : MOM_read_data
+use MOM_EOS, only : EOS_type, calculate_density, calculate_density_derivs
 use MOM_unit_scaling, only : unit_scale_type
 
 use netcdf
@@ -15,7 +16,7 @@ implicit none ; private
 
 #include <MOM_memory.h>
 
-public tracer_Z_init
+public tracer_Z_init, tracer_Z_init_array, find_interfaces, determine_temperature
 
 ! A note on unit descriptions in comments: MOM6 uses units that can be rescaled for dimensional
 ! consistency testing. These are noted in comments with units like Z, H, L, and T, along with
@@ -274,6 +275,112 @@ function tracer_Z_init(tr, h, filename, tr_name, G, US, missing_val, land_val)
 
 end function tracer_Z_init
 
+!> Layer model routine for remapping tracers
+!! from pseudo-z coordinates into layers defined
+!! by target interface positions.
+subroutine tracer_z_init_array(tr_in, z_edges, e, nkml, nkbl, land_fill, wet, nlay, nlevs, &
+                       eps_z, tr)
+  real, dimension(:,:,:),           intent(in) :: tr_in !< The z-space array of tracer concentrations that is read in.
+  real, dimension(size(tr_in,3)+1), intent(in) :: z_edges !< The depths of the cell edges in the input z* data
+                                                       !! [Z ~> m or m]
+  integer,                          intent(in) :: nlay !< The number of vertical layers in the target grid
+  real, dimension(size(tr_in,1),size(tr_in,2),nlay+1), &
+                                    intent(in) :: e !< The depths of the target layer interfaces [Z ~> m or m]
+  integer,                          intent(in) :: nkml !< The number of mixed layers
+  integer,                          intent(in) :: nkbl !< The number of buffer layers
+  real,                             intent(in) :: land_fill !< fill in data over land (1)
+  real, dimension(size(tr_in,1),size(tr_in,2)), &
+                                    intent(in) :: wet !< The wet mask for the source data (valid points)
+  integer, dimension(size(tr_in,1),size(tr_in,2)), &
+                                    intent(in) :: nlevs !< The number of input levels with valid data
+  real,                             intent(in) :: eps_z ! A negligibly thin layer thickness [Z ~> m].
+  real, dimension(size(tr_in,1),size(tr_in,2),nlay), intent(out) :: tr !< tracers in layer space
+
+  ! Local variables
+  real, dimension(size(tr_in,3)) :: tr_1d !< a copy of the input tracer concentrations in a column.
+  real, dimension(nlay+1) :: e_1d ! A 1-d column of intreface heights, in the same units as e.
+  real, dimension(nlay) :: tr_    ! A 1-d column of tracer concentrations
+  integer :: n,i,j,k,l,nx,ny,nz,nt,kz
+  integer :: k_top,k_bot,k_bot_prev,kk,kstart
+  real    :: sl_tr    ! The tracer concentration slope times the layer thickness, in tracer units.
+  real, dimension(size(tr_in,3)) :: wt !< The fractional weight for each layer in the range between z1 and z2
+  real, dimension(size(tr_in,3)) :: z1, z2 ! z1 and z2 are the fractional depths of the top and bottom
+                                  ! limits of the part of a z-cell that contributes to a layer, relative
+                                  ! to the cell center and normalized by the cell thickness [nondim].
+                                  ! Note that -1/2 <= z1 <= z2 <= 1/2.
+
+  nx = size(tr_in,1); ny=size(tr_in,2); nz = size(tr_in,3)
+
+
+  do j=1,ny
+    i_loop: do i=1,nx
+      if (nlevs(i,j) == 0 .or. wet(i,j) == 0.) then
+        tr(i,j,:) = land_fill
+        cycle i_loop
+      endif
+
+      do k=1,nz
+        tr_1d(k) = tr_in(i,j,k)
+      enddo
+
+      do k=1,nlay+1
+        e_1d(k) = e(i,j,k)
+      enddo
+      k_bot = 1 ; k_bot_prev = -1
+      do k=1,nlay
+        if (e_1d(k+1) > z_edges(1)) then
+          tr(i,j,k) = tr_1d(1)
+        elseif (e_1d(k) < z_edges(nlevs(i,j)+1)) then
+          tr(i,j,k) = tr_1d(nlevs(i,j))
+
+        else
+          kstart=k_bot
+          call find_overlap(z_edges, e_1d(k), e_1d(k+1), nlevs(i,j), &
+                            kstart, k_top, k_bot, wt, z1, z2)
+          kz = k_top
+          sl_tr=0.0; ! cur_tr=0.0
+          if (kz /= k_bot_prev) then
+            ! Calculate the intra-cell profile.
+            if ((kz < nlevs(i,j)) .and. (kz > 1)) then
+              sl_tr = find_limited_slope(tr_1d, z_edges, kz)
+            endif
+          endif
+          if (kz > nlevs(i,j)) kz = nlevs(i,j)
+          ! This is the piecewise linear form.
+          tr(i,j,k) = wt(kz) * (tr_1d(kz) + 0.5*sl_tr*(z2(kz) + z1(kz)))
+          ! For the piecewise parabolic form add the following...
+          !     + C1_3*wt(kz) * cur_tr*(z2(kz)**2 + z2(kz)*z1(kz) + z1(kz)**2))
+          do kz=k_top+1,k_bot-1
+            tr(i,j,k) = tr(i,j,k) + wt(kz)*tr_1d(kz)
+          enddo
+
+          if (k_bot > k_top) then
+            kz = k_bot
+            ! Calculate the intra-cell profile.
+            sl_tr = 0.0 ! ; cur_tr = 0.0
+            if ((kz < nlevs(i,j)) .and. (kz > 1)) then
+              sl_tr = find_limited_slope(tr_1d, z_edges, kz)
+            endif
+            ! This is the piecewise linear form.
+            tr(i,j,k) = tr(i,j,k) + wt(kz) * &
+                 (tr_1d(kz) + 0.5*sl_tr*(z2(kz) + z1(kz)))
+            ! For the piecewise parabolic form add the following...
+            !     + C1_3*cur_tr*(z2(kz)**2 + z2(kz)*z1(kz) + z1(kz)**2))
+          endif
+          k_bot_prev = k_bot
+
+        endif
+      enddo ! k-loop
+
+      do k=2,nlay  ! simply fill vanished layers with adjacent value
+        if (e_1d(k)-e_1d(k+1) <= eps_z) tr(i,j,k)=tr(i,j,k-1)
+      enddo
+
+    enddo i_loop
+  enddo
+
+end subroutine tracer_z_init_array
+
 !> This subroutine reads the vertical coordinate data for a field from a NetCDF file.
 !! It also might read the missing value attribute for that same field.
 subroutine read_Z_edges(filename, tr_name, z_edges, nz_out, has_edges, &
@@ -498,5 +605,276 @@ function find_limited_slope(val, e, k) result(slope)
 
 end function find_limited_slope
 
+!> Find interface positions corresponding to density profile
+function find_interfaces(rho, zin, Rb, depth, nlevs, nkml, nkbl, hml, debug, eps_z, eps_rho) result(zi)
+  real, dimension(:,:,:), &
+                      intent(in) :: rho   !< potential density in z-space [kg m-3 or R ~> kg m-3]
+  real, dimension(size(rho,3)), &
+                      intent(in) :: zin   !< Input data levels [m or Z ~> m].
+  real, dimension(:), intent(in) :: Rb    !< target interface densities [kg m-3 or R ~> kg m-3]
+  real, dimension(size(rho,1),size(rho,2)), &
+                      intent(in) :: depth !< ocean depth [Z ~> m].
+  integer, dimension(size(rho,1),size(rho,2)), &
+            optional, intent(in) :: nlevs !< number of valid points in each column
+  logical,  optional, intent(in) :: debug !< optional debug flag
+  integer,  optional, intent(in) :: nkml  !< number of mixed layer pieces
+  integer,  optional, intent(in) :: nkbl  !< number of buffer layer pieces
+  real,     optional, intent(in) :: hml   !< mixed layer depth [Z ~> m].
+  real,     optional, intent(in) :: eps_z !< A negligibly small layer thickness [m or Z ~> m].
+  real,     optional, intent(in) :: eps_rho !< A negligibly small density difference [kg m-3 or R ~> kg m-3].
+  real, dimension(size(rho,1),size(rho,2),size(Rb,1)) :: zi !< The returned interface, in the same units az zin.
+
+  ! Local variables
+  real, dimension(size(rho,1),size(rho,3)) :: rho_ ! A slice of densities [R ~> kg m-3]
+  real, dimension(size(rho,1)) :: depth_
+  logical :: unstable
+  integer :: dir
+  integer, dimension(size(rho,1),size(Rb,1)) :: ki_
+  real, dimension(size(rho,1),size(Rb,1)) :: zi_
+  integer, dimension(size(rho,1),size(rho,2)) :: nlevs_data
+  integer, dimension(size(rho,1)) :: lo, hi
+  real :: slope,rsm,drhodz,hml_
+  integer :: n,i,j,k,l,nx,ny,nz,nt
+  integer :: nlay,kk,nkml_,nkbl_
+  logical :: debug_ = .false.
+  real    :: epsln_Z    ! A negligibly thin layer thickness [m or Z ~> m].
+  real    :: epsln_rho  ! A negligibly small density change [kg m-3 or R ~> kg m-3].
+  real, parameter :: zoff=0.999
+
+  nlay=size(Rb)-1
+
+  zi(:,:,:) = 0.0
+
+  if (PRESENT(debug)) debug_=debug
+
+  nx = size(rho,1); ny=size(rho,2); nz = size(rho,3)
+  nlevs_data(:,:) = size(rho,3)
+
+  nkml_ = 0 ;  if (PRESENT(nkml)) nkml_ = max(0, nkml)
+  nkbl_ = 0 ;  if (PRESENT(nkbl)) nkbl_ = max(0, nkbl)
+  hml_ = 0.0 ; if (PRESENT(hml)) hml_ = hml
+  epsln_Z = 1.0e-10 ; if (PRESENT(eps_z)) epsln_Z = eps_z
+  epsln_rho = 1.0e-10 ; if (PRESENT(eps_rho)) epsln_rho = eps_rho
+
+  if (PRESENT(nlevs)) then
+    nlevs_data(:,:) = nlevs(:,:)
+  endif
+
+  do j=1,ny
+    rho_(:,:) = rho(:,j,:)
+    i_loop: do i=1,nx
+      if (debug_) then
+        print *,'looking for interfaces, i,j,nlevs= ',i,j,nlevs_data(i,j)
+        print *,'initial density profile= ', rho_(i,:)
+      endif
+      unstable=.true.
+      dir=1
+      do while (unstable)
+        unstable=.false.
+        if (dir == 1) then
+          do k=2,nlevs_data(i,j)-1
+            if (rho_(i,k) - rho_(i,k-1) < 0.0 ) then
+              if (k == 2) then
+                rho_(i,k-1) = rho_(i,k)-epsln_rho
+              else
+                drhodz = (rho_(i,k+1)-rho_(i,k-1)) / (zin(k+1)-zin(k-1))
+                if (drhodz < 0.0) unstable=.true.
+                rho_(i,k) = rho_(i,k-1) + drhodz*zoff*(zin(k)-zin(k-1))
+              endif
+            endif
+          enddo
+          dir = -1*dir
+        else
+          do k=nlevs_data(i,j)-1,2,-1
+            if (rho_(i,k+1) - rho_(i,k) < 0.0) then
+              if (k == nlevs_data(i,j)-1) then
+                rho_(i,k+1) = rho_(i,k-1)+epsln_rho
+              else
+                drhodz = (rho_(i,k+1)-rho_(i,k-1))/(zin(k+1)-zin(k-1))
+                if (drhodz  < 0.0) unstable=.true.
+                rho_(i,k) = rho_(i,k+1)-drhodz*(zin(k+1)-zin(k))
+              endif
+            endif
+          enddo
+          dir = -1*dir
+        endif
+      enddo
+      if (debug_) then
+        print *,'final density profile= ', rho_(i,:)
+      endif
+    enddo i_loop
+
+    ki_(:,:) = 0
+    zi_(:,:) = 0.0
+    depth_(:) = -1.0*depth(:,j)
+    lo(:) = 1
+    hi(:) = nlevs_data(:,j)
+    ki_ = bisect_fast(rho_, Rb, lo, hi)
+    ki_(:,:) = max(1, ki_(:,:)-1)
+    do i=1,nx
+      do l=2,nlay
+        slope = (zin(ki_(i,l)+1) - zin(ki_(i,l))) / max(rho_(i,ki_(i,l)+1) - rho_(i,ki_(i,l)),epsln_rho)
+        zi_(i,l) = -1.0*(zin(ki_(i,l)) + slope*(Rb(l)-rho_(i,ki_(i,l))))
+        zi_(i,l) = max(zi_(i,l), depth_(i))
+        zi_(i,l) = min(zi_(i,l), -1.0*hml_)
+      enddo
+      zi_(i,nlay+1) = depth_(i)
+      do l=2,nkml_+1
+        zi_(i,l) = max(hml_*((1.0-real(l))/real(nkml_)), depth_(i))
+      enddo
+      do l=nlay,nkml_+2,-1
+        if (zi_(i,l) < zi_(i,l+1) + epsln_Z) zi_(i,l) = zi_(i,l+1) + epsln_Z
+        if (zi_(i,l) > -1.0*hml_)  zi_(i,l) = max(-1.0*hml_, depth_(i))
+      enddo
+    enddo
+    zi(:,j,:) = zi_(:,:)
+  enddo
+
+end function find_interfaces
+
+!> This subroutine determines the potential temperature and salinity that
+!! is consistent with the target density using provided initial guess
+subroutine determine_temperature(temp, salt, R, p_ref, niter, land_fill, h, k_start, eos)
+  real, dimension(:,:,:),        intent(inout) :: temp !< potential temperature [degC]
+  real, dimension(:,:,:),        intent(inout) :: salt !< salinity [PSU]
+  real, dimension(size(temp,3)), intent(in)    :: R !< desired potential density [kg m-3].
+  real,                          intent(in)    :: p_ref !< reference pressure [Pa].
+  integer,                       intent(in)    :: niter !< maximum number of iterations
+  integer,                       intent(in)    :: k_start !< starting index (i.e. below the buffer layer)
+  real,                          intent(in)    :: land_fill !< land fill value
+  real, dimension(:,:,:),        intent(in)    :: h   !< layer thickness, used only to avoid working on massless layers
+  type(eos_type),                pointer       :: eos !< seawater equation of state control structure
+
+  real, parameter :: T_max = 31.0, T_min = -2.0
+  ! Local variables (All of which need documentation!)
+  real(kind=8), dimension(size(temp,1),size(temp,3)) :: T, S, dT, dS, rho, hin
+  real(kind=8), dimension(size(temp,1),size(temp,3)) :: drho_dT, drho_dS
+  real(kind=8), dimension(size(temp,1)) :: press
+  integer :: nx, ny, nz, nt, i, j, k, n, itt
+  real    :: dT_dS_gauge  ! The relative penalizing of temperature to salinity changes when
+                          ! minimizing property changes while correcting density [degC ppt-1].
+  real    :: I_denom      ! The inverse of the magnitude squared of the density gradient in
+                          ! T-S space streched with dT_dS_gauge [m6 kg-2 ppt-1]
+  logical :: adjust_salt, old_fit
+  real, parameter :: S_min = 0.5, S_max=65.0
+  real, parameter :: tol_T=1.e-4, tol_S=1.e-4, tol_rho=1.e-4
+  real, parameter :: max_t_adj=1.0, max_s_adj = 0.5
+
+  old_fit = .true.   ! reproduces siena behavior
+
+  ! ### The whole determine_temperature subroutine needs to be reexamined, both the algorithms
+  !     and the extensive use of hard-coded dimensional parameters.
+
+  ! We will switch to the newer method which simultaneously adjusts
+  ! temp and salt based on the ratio of the thermal and haline coefficients, once it is tested.
+
+  nx=size(temp,1) ; ny=size(temp,2) ; nz=size(temp,3)
+
+  press(:) = p_ref
+
+  do j=1,ny
+    dS(:,:) = 0. ! Needs to be zero everywhere since there is a maxval(abs(dS)) later...
+    T=temp(:,j,:)
+    S=salt(:,j,:)
+    hin=h(:,j,:)
+    dT=0.0
+    adjust_salt = .true.
+    iter_loop: do itt = 1,niter
+      do k=1, nz
+        call calculate_density(T(:,k), S(:,k), press, rho(:,k), 1, nx, eos)
+        call calculate_density_derivs(T(:,k), S(:,k), press, drho_dT(:,k), drho_dS(:,k), 1, nx, eos)
+      enddo
+      do k=k_start,nz ; do i=1,nx
+
+!       if (abs(rho(i,k)-R(k))>tol .and. hin(i,k)>epsln .and. abs(T(i,k)-land_fill) < epsln) then
+        if (abs(rho(i,k)-R(k))>tol_rho) then
+          if (old_fit) then
+            dT(i,k) = max(min((R(k)-rho(i,k)) / drho_dT(i,k), max_t_adj), -max_t_adj)
+            T(i,k) = max(min(T(i,k)+dT(i,k), T_max), T_min)
+          else
+            dT_dS_gauge = 10.0  ! 10 degC is weighted equivalently to 1 ppt.
+            I_denom = 1.0 / (drho_dS(i,k)**2 + dT_dS_gauge**2*drho_dT(i,k)**2)
+            dS(i,k) = (R(k)-rho(i,k)) * drho_dS(i,k) * I_denom
+            dT(i,k) = (R(k)-rho(i,k)) * dT_dS_gauge**2*drho_dT(i,k) * I_denom
+
+            T(i,k) = max(min(T(i,k)+dT(i,k), T_max), T_min)
+            S(i,k) = max(min(S(i,k)+dS(i,k), S_max), S_min)
+          endif
+        endif
+      enddo ; enddo
+      if (maxval(abs(dT)) < tol_T) then
+         adjust_salt = .false.
+         exit iter_loop
+      endif
+    enddo iter_loop
+
+    if (adjust_salt .and. old_fit) then ; do itt = 1,niter
+      do k=1, nz
+        call calculate_density(T(:,k),S(:,k),press,rho(:,k),1,nx,eos)
+        call calculate_density_derivs(T(:,k),S(:,k),press,drho_dT(:,k),drho_dS(:,k),1,nx,eos)
+      enddo
+      do k=k_start,nz ; do i=1,nx
+!       if (abs(rho(i,k)-R(k))>tol .and. hin(i,k)>epsln .and. abs(T(i,k)-land_fill) < epsln ) then
+        if (abs(rho(i,k)-R(k)) > tol_rho) then
+          dS(i,k) = max(min((R(k)-rho(i,k)) / drho_dS(i,k), max_s_adj), -max_s_adj)
+          S(i,k) = max(min(S(i,k)+dS(i,k), S_max), S_min)
+        endif
+      enddo ; enddo
+      if (maxval(abs(dS)) < tol_S) exit
+    enddo ; endif
+
+    temp(:,j,:)=T(:,:)
+    salt(:,j,:)=S(:,:)
+  enddo
+
+end subroutine determine_temperature
+
+!> Return the index where to insert item x in list a, assuming a is sorted.
+!! The return values [i] is such that all e in a[:i-1] have e <= x, and all e in
+!! a[i:] have e > x. So if x already appears in the list, will
+!! insert just after the rightmost x already there.
+!! Optional args lo (default 1) and hi (default len(a)) bound the
+!! slice of a to be searched.
+function bisect_fast(a, x, lo, hi) result(bi_r)
+  real, dimension(:,:), intent(in) :: a !< Sorted list
+  real, dimension(:), intent(in) :: x !< Item to be inserted
+  integer, dimension(size(a,1)), optional, intent(in) :: lo !< Lower bracket of optional range to search
+  integer, dimension(size(a,1)), optional, intent(in) :: hi !< Upper bracket of optional range to search
+  integer, dimension(size(a,1),size(x,1))  :: bi_r
+
+  integer :: mid,num_x,num_a,i
+  integer, dimension(size(a,1))  :: lo_,hi_,lo0,hi0
+  integer :: nprofs,j
+
+  lo_=1;hi_=size(a,2);num_x=size(x,1);bi_r=-1;nprofs=size(a,1)
+
+  if (PRESENT(lo)) then
+     where (lo>0) lo_=lo
+  endif
+  if (PRESENT(hi)) then
+     where (hi>0) hi_=hi
+  endif
+
+  lo0=lo_;hi0=hi_
+
+  do j=1,nprofs
+    do i=1,num_x
+      lo_=lo0;hi_=hi0
+      do while (lo_(j) < hi_(j))
+        mid = (lo_(j)+hi_(j))/2
+        if (x(i) < a(j,mid)) then
+           hi_(j) = mid
+        else
+           lo_(j) = mid+1
+        endif
+      enddo
+      bi_r(j,i)=lo_(j)
+    enddo
+  enddo
+
+
+  return
+
+end function bisect_fast
 
 end module MOM_tracer_Z_init
