a43b480069b20a403e2f7527de40b3cbd265de41
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2020-06-12 01:11:05 +0000

    Documented FMS_coupler_util API

--- config_src/external_APIs/GFDL_ocean_BGC/FMS_coupler_util.F90
+++ config_src/external_APIs/GFDL_ocean_BGC/FMS_coupler_util.F90
@@ -8,24 +8,28 @@ public :: extract_coupler_values, set_coupler_values
 
 contains
 
+!> Get element and index of a boundary condition
 subroutine extract_coupler_values(BC_struc, BC_index, BC_element, array_out, ilb, jlb, &
                                   is, ie, js, je, conversion)
-  real, dimension(ilb:,jlb:),intent(out) :: array_out
-  integer,                   intent(in)  :: ilb, jlb
-  type(coupler_2d_bc_type),  intent(in)  :: BC_struc
-  integer,                   intent(in)  :: BC_index, BC_element
-  integer,        optional,  intent(in)  :: is, ie, js, je
-  real,           optional,  intent(in)  :: conversion
+  real, dimension(ilb:,jlb:),intent(out) :: array_out !< The array being filled with the input values
+  integer,                   intent(in)  :: ilb, jlb !< Lower bounds
+  type(coupler_2d_bc_type),  intent(in)  :: BC_struc !< A number that every element is multiplied by
+  integer,                   intent(in)  :: BC_index !< The boundary condition number being extracted
+  integer,                   intent(in)  :: BC_element !< The element of the boundary condition being extracted
+  integer,        optional,  intent(in)  :: is, ie, js, je !< The i- and j- limits of array_out to be filled
+  real,           optional,  intent(in)  :: conversion !< A number that every element is multiplied by
 end subroutine extract_coupler_values
 
+!> Set element and index of a boundary condition
 subroutine set_coupler_values(array_in, BC_struc, BC_index, BC_element, ilb, jlb,&
                               is, ie, js, je, conversion)
-  real, dimension(ilb:,jlb:), intent(in)  :: array_in
-  integer,                  intent(in)    :: ilb, jlb
-  type(coupler_2d_bc_type), intent(inout) :: BC_struc
-  integer,                  intent(in)    :: BC_index, BC_element
-  integer,        optional, intent(in)    :: is, ie, js, je
-  real,           optional, intent(in)    :: conversion
+  real, dimension(ilb:,jlb:), intent(in)  :: array_in !< The array containing the values to load into the BC
+  integer,                  intent(in)    :: ilb, jlb !< Lower bounds
+  type(coupler_2d_bc_type), intent(inout) :: BC_struc !< The type into which the data is being loaded
+  integer,                  intent(in)    :: BC_index !< The boundary condition number being extracted
+  integer,                  intent(in)    :: BC_element !< The element of the boundary condition being extracted
+  integer,        optional, intent(in)    :: is, ie, js, je !< The i- and j- limits of array_out to be filled
+  real,           optional, intent(in)    :: conversion !< A number that every element is multiplied by
 end subroutine set_coupler_values
 
 end module FMS_coupler_util
