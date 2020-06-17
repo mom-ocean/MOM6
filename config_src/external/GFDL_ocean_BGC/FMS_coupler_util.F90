f11d301679eb7411afaa02b9a24be130183d77c1
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2020-06-17 22:10:47 +0000

    Corrected docs for FMS_coupler.F90
    - @Hallberg-NOAA pointed out some obviously wrong documentation
      of arguments.

--- config_src/external/GFDL_ocean_BGC/FMS_coupler_util.F90
+++ config_src/external/GFDL_ocean_BGC/FMS_coupler_util.F90
@@ -13,7 +13,7 @@ subroutine extract_coupler_values(BC_struc, BC_index, BC_element, array_out, ilb
                                   is, ie, js, je, conversion)
   real, dimension(ilb:,jlb:),intent(out) :: array_out !< The array being filled with the input values
   integer,                   intent(in)  :: ilb, jlb !< Lower bounds
-  type(coupler_2d_bc_type),  intent(in)  :: BC_struc !< A number that every element is multiplied by
+  type(coupler_2d_bc_type),  intent(in)  :: BC_struc !< The type from which the data is being extracted
   integer,                   intent(in)  :: BC_index !< The boundary condition number being extracted
   integer,                   intent(in)  :: BC_element !< The element of the boundary condition being extracted
   integer,        optional,  intent(in)  :: is, ie, js, je !< The i- and j- limits of array_out to be filled
@@ -26,8 +26,8 @@ subroutine set_coupler_values(array_in, BC_struc, BC_index, BC_element, ilb, jlb
   real, dimension(ilb:,jlb:), intent(in)  :: array_in !< The array containing the values to load into the BC
   integer,                  intent(in)    :: ilb, jlb !< Lower bounds
   type(coupler_2d_bc_type), intent(inout) :: BC_struc !< The type into which the data is being loaded
-  integer,                  intent(in)    :: BC_index !< The boundary condition number being extracted
-  integer,                  intent(in)    :: BC_element !< The element of the boundary condition being extracted
+  integer,                  intent(in)    :: BC_index !< The boundary condition number being set
+  integer,                  intent(in)    :: BC_element !< The element of the boundary condition being set
   integer,        optional, intent(in)    :: is, ie, js, je !< The i- and j- limits of array_out to be filled
   real,           optional, intent(in)    :: conversion !< A number that every element is multiplied by
 end subroutine set_coupler_values
