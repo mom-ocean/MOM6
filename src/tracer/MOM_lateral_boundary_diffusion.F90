a49954a73ce1297cf814b9cf1f88115ab7f94dcc
Gustavo Marques <gmarques@ucar.edu>
2020-07-07 16:14:02 -0600

    comment calls to BLD schemes

--- src/tracer/MOM_lateral_boundary_diffusion.F90
+++ src/tracer/MOM_lateral_boundary_diffusion.F90
@@ -167,9 +167,9 @@ subroutine lateral_boundary_diffusion(G, GV, US, h, Coef_x, Coef_y, dt, Reg, CS)
 
   Idt = 1./dt
   hbl(:,:) = 100.
-  hbl(4:6,:) = 500.
-  if (ASSOCIATED(CS%KPP_CSp)) call KPP_get_BLD(CS%KPP_CSp, hbl, G)
-  if (ASSOCIATED(CS%energetic_PBL_CSp)) call energetic_PBL_get_MLD(CS%energetic_PBL_CSp, hbl, G, US)
+  hbl(4:6,:) = 100.
+  !if (ASSOCIATED(CS%KPP_CSp)) call KPP_get_BLD(CS%KPP_CSp, hbl, G)
+  !if (ASSOCIATED(CS%energetic_PBL_CSp)) call energetic_PBL_get_MLD(CS%energetic_PBL_CSp, hbl, G, US)
 
   call pass_var(hbl,G%Domain)
   do m = 1,Reg%ntr
--- src/tracer/MOM_neutral_diffusion.F90
+++ src/tracer/MOM_neutral_diffusion.F90
@@ -316,9 +316,9 @@ subroutine neutral_diffusion_calc_coeffs(G, GV, US, h, T, S, CS, p_surf)
   ! Check if hbl needs to be extracted
   if (CS%interior_only) then
     hbl(:,:) = 100.
-    hbl(4:6,:) = 500.
-    if (ASSOCIATED(CS%KPP_CSp)) call KPP_get_BLD(CS%KPP_CSp, hbl, G)
-    if (ASSOCIATED(CS%energetic_PBL_CSp)) call energetic_PBL_get_MLD(CS%energetic_PBL_CSp, hbl, G, US)
+    hbl(4:6,:) = 100.
+    !if (ASSOCIATED(CS%KPP_CSp)) call KPP_get_BLD(CS%KPP_CSp, hbl, G)
+    !if (ASSOCIATED(CS%energetic_PBL_CSp)) call energetic_PBL_get_MLD(CS%energetic_PBL_CSp, hbl, G, US)
     call pass_var(hbl,G%Domain)
     ! get k-indices and zeta
     do j=G%jsc-1, G%jec+1 ; do i=G%isc-1,G%iec+1
