d8ae09f80e193a1d1ab94be4f03b1856aa31b668
Gustavo Marques <gmarques@ucar.edu>
2020-09-04 15:54:13 -0600

    Rename lateral_boundary_diffusion_CS to lbd_CS

--- src/tracer/MOM_tracer_hor_diff.F90
+++ src/tracer/MOM_tracer_hor_diff.F90
@@ -23,7 +23,7 @@ use MOM_MEKE_types,            only : MEKE_type
 use MOM_neutral_diffusion,     only : neutral_diffusion_init, neutral_diffusion_end
 use MOM_neutral_diffusion,     only : neutral_diffusion_CS
 use MOM_neutral_diffusion,     only : neutral_diffusion_calc_coeffs, neutral_diffusion
-use MOM_lateral_boundary_diffusion, only : lateral_boundary_diffusion_CS, lateral_boundary_diffusion_init
+use MOM_lateral_boundary_diffusion, only : lbd_CS, lateral_boundary_diffusion_init
 use MOM_lateral_boundary_diffusion, only : lateral_boundary_diffusion
 use MOM_tracer_registry,       only : tracer_registry_type, tracer_type, MOM_tracer_chksum
 use MOM_unit_scaling,          only : unit_scale_type
@@ -64,7 +64,7 @@ type, public :: tracer_hor_diff_CS ; private
   logical :: recalc_neutral_surf   !< If true, recalculate the neutral surfaces if CFL has been
                                    !! exceeded
   type(neutral_diffusion_CS), pointer :: neutral_diffusion_CSp => NULL() !< Control structure for neutral diffusion.
-  type(lateral_boundary_diffusion_CS), pointer :: lateral_boundary_diffusion_CSp => NULL() !< Control structure for
+  type(lbd_CS), pointer :: lateral_boundary_diffusion_CSp => NULL() !< Control structure for
                                                                                      !! lateral boundary mixing.
   type(diag_ctrl), pointer :: diag => NULL() !< A structure that is used to
                                    !! regulate the timing of diagnostic output.
