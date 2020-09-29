mom-ocean/MOM6

commit bb83604f23b306ecaccac5cf54b6d5aed435d5e7

Consolidates initialization of frac_shelf_h
  - This is stored in the forcing type (unrotated)
    the MOM_CS (rotated).
  - Now this field is updated by a query to the ice shelf module
    prior to calling MOM_state_initialization .

config_src/solo_driver/MOM_driver.F90
@@ -299,16 +299,29 @@ program MOM_main
     ! In this case, the segment starts at a time fixed by ocean_solo.res
     segment_start_time = set_date(date(1),date(2),date(3),date(4),date(5),date(6))
     Time = segment_start_time
-    call initialize_MOM(Time, Start_time, param_file, dirs, MOM_CSp, restart_CSp, &
-                        segment_start_time, offline_tracer_mode=offline_tracer_mode, &
-                        diag_ptr=diag, tracer_flow_CSp=tracer_flow_CSp)
   else
     ! In this case, the segment starts at a time read from the MOM restart file
     ! or left as Start_time by MOM_initialize.
     Time = Start_time
+  endif
+
+  call get_param(param_file, mod_name, "ICE_SHELF", use_ice_shelf, &
+                 "If true, enables the ice shelf model.", default=.false.)
+  if (use_ice_shelf) then
+    ! These arrays are not initialized in most solo cases, but are needed
+    ! when using an ice shelf
+    call initialize_ice_shelf(param_file, grid, Time, ice_shelf_CSp, &
+                              diag, forces, fluxes, sfc_state)
+  endif
+
+  if (sum(date) >= 0) then
+    call initialize_MOM(Time, Start_time, param_file, dirs, MOM_CSp, restart_CSp, &
+                        segment_start_time, offline_tracer_mode=offline_tracer_mode, &
+                        diag_ptr=diag, tracer_flow_CSp=tracer_flow_CSp,ice_shelf_CSp=ice_shelf_CSp)
+  else
     call initialize_MOM(Time, Start_time, param_file, dirs, MOM_CSp, restart_CSp, &
                         offline_tracer_mode=offline_tracer_mode, diag_ptr=diag, &
-                        tracer_flow_CSp=tracer_flow_CSp)
+                        tracer_flow_CSp=tracer_flow_CSp,ice_shelf_CSp=ice_shelf_CSp)
   endif
 
   call get_MOM_state_elements(MOM_CSp, G=grid, GV=GV, US=US, C_p_scaled=fluxes%C_p)
@@ -322,14 +335,6 @@ program MOM_main
                             surface_forcing_CSp, tracer_flow_CSp)
   call callTree_waypoint("done surface_forcing_init")
 
-  call get_param(param_file, mod_name, "ICE_SHELF", use_ice_shelf, &
-                 "If true, enables the ice shelf model.", default=.false.)
-  if (use_ice_shelf) then
-    ! These arrays are not initialized in most solo cases, but are needed
-    ! when using an ice shelf
-    call initialize_ice_shelf(param_file, grid, Time, ice_shelf_CSp, &
-                              diag, forces, fluxes, sfc_state)
-  endif
 
   call get_param(param_file,mod_name,"USE_WAVES",Use_Waves,&
        "If true, enables surface wave modules.",default=.false.)

src/ice_shelf/MOM_ice_shelf.F90
@@ -65,7 +65,7 @@ implicit none ; private
 #  define GRID_SYM_ .false.
 #endif
 
-public shelf_calc_flux, initialize_ice_shelf, ice_shelf_end
+public shelf_calc_flux, initialize_ice_shelf, ice_shelf_end, ice_shelf_query
 public ice_shelf_save_restart, solo_step_ice_shelf, add_shelf_forces
 
 ! A note on unit descriptions in comments: MOM6 uses units that can be rescaled for dimensional
@@ -1966,6 +1966,28 @@ subroutine update_shelf_mass(G, US, CS, ISS, Time)
 
 end subroutine update_shelf_mass
 
+!> Save the ice shelf restart file
+subroutine ice_shelf_query(CS, G, frac_shelf_h)
+  type(ice_shelf_CS),         pointer    :: CS !< ice shelf control structure
+  type(ocean_grid_type), intent(in)      :: G  !< A pointer to an ocean grid control structure.
+  real, optional, dimension(SZI_(G),SZJ_(G))  :: frac_shelf_h !<
+                                      !< Ice shelf area fraction [nodim].
+
+  logical :: do_frac=.false.
+  integer :: i,j
+
+  if (present(frac_shelf_h)) do_frac=.true.
+
+  if (do_frac) then
+     do j=G%jsd,G%jed
+       do i=G%isd,G%ied
+         frac_shelf_h(i,j)=0.0
+         if (G%areaT(i,j)>0.) frac_shelf_h(i,j) = CS%ISS%area_shelf_h(i,j) / G%areaT(i,j)
+       enddo
+     enddo
+   endif
+
+end subroutine ice_shelf_query
 !> Save the ice shelf restart file
 subroutine ice_shelf_save_restart(CS, Time, directory, time_stamped, filename_suffix)
   type(ice_shelf_CS),         pointer    :: CS !< ice shelf control structure

src/core/MOM.F90
@@ -141,6 +141,7 @@ use MOM_offline_main,          only : offline_redistribute_residual, offline_dia
 use MOM_offline_main,          only : offline_fw_fluxes_into_ocean, offline_fw_fluxes_out_ocean
 use MOM_offline_main,          only : offline_advection_layer, offline_transport_end
 use MOM_ALE,                   only : ale_offline_tracer_final, ALE_main_offline
+use MOM_ice_shelf,             only : ice_shelf_CS, ice_shelf_query
 
 implicit none ; private
 
@@ -1598,7 +1599,7 @@ end subroutine step_offline
 !! initializing the ocean state variables, and initializing subsidiary modules
 subroutine initialize_MOM(Time, Time_init, param_file, dirs, CS, restart_CSp, &
                           Time_in, offline_tracer_mode, input_restart_file, diag_ptr, &
-                          count_calls, tracer_flow_CSp)
+                          count_calls, tracer_flow_CSp,  ice_shelf_CSp)
   type(time_type), target,   intent(inout) :: Time        !< model time, set in this routine
   type(time_type),           intent(in)    :: Time_init   !< The start time for the coupled model's calendar
   type(param_file_type),     intent(out)   :: param_file  !< structure indicating parameter file to parse
@@ -1619,6 +1620,7 @@ subroutine initialize_MOM(Time, Time_init, param_file, dirs, CS, restart_CSp, &
   logical,         optional, intent(in)    :: count_calls !< If true, nstep_tot counts the number of
                                                           !! calls to step_MOM instead of the number of
                                                           !! dynamics timesteps.
+  type(ice_shelf_CS), optional,     pointer :: ice_shelf_CSp !< A pointer to an ice shelf control structure
   ! local variables
   type(ocean_grid_type),  pointer :: G => NULL()    ! A pointer to the metric grid use for the run
   type(ocean_grid_type),  pointer :: G_in => NULL() ! Pointer to the input grid
@@ -2030,17 +2032,22 @@ subroutine initialize_MOM(Time, Time_init, param_file, dirs, CS, restart_CSp, &
       "initialize_MOM: A bulk mixed layer can only be used with T & S as "//&
       "state variables. Add USE_EOS = True to MOM_input.")
 
-  call get_param(param_file, 'MOM', "ICE_SHELF", use_ice_shelf, default=.false., do_not_log=.true.)
-  if (use_ice_shelf) then
-     inputdir = "." ;  call get_param(param_file, 'MOM', "INPUTDIR", inputdir)
-     inputdir = slasher(inputdir)
-     call get_param(param_file, 'MOM', "ICE_THICKNESS_FILE", ice_shelf_file, &
-                    "The file from which the ice bathymetry and area are read.", &
-                    fail_if_missing=.true.)
-     call get_param(param_file, 'MOM', "ICE_AREA_VARNAME", area_varname, &
-                    "The name of the area variable in ICE_THICKNESS_FILE.", &
-                    fail_if_missing=.true.)
+  use_ice_shelf=.false.
+  if (present(ice_shelf_CSp)) then
+    if (associated(ice_shelf_CSp)) use_ice_shelf=.true.
   endif
+
+  ! call get_param(param_file, 'MOM', "ICE_SHELF", use_ice_shelf, default=.false., do_not_log=.true.)
+  ! if (use_ice_shelf) then
+  !    inputdir = "." ;  call get_param(param_file, 'MOM', "INPUTDIR", inputdir)
+  !    inputdir = slasher(inputdir)
+  !    call get_param(param_file, 'MOM', "ICE_THICKNESS_FILE", ice_shelf_file, &
+  !                   "The file from which the ice bathymetry and area are read.", &
+  !                   fail_if_missing=.true.)
+  !    call get_param(param_file, 'MOM', "ICE_AREA_VARNAME", area_varname, &
+  !                   "The name of the area variable in ICE_THICKNESS_FILE.", &
+  !                   fail_if_missing=.true.)
+  ! endif
 
 
   CS%ensemble_ocean=.false.
@@ -2387,26 +2394,32 @@ subroutine initialize_MOM(Time, Time_init, param_file, dirs, CS, restart_CSp, &
 
     if (use_ice_shelf) then
       ! TODO: Turn this into a function?
-      if (.not. file_exists(ice_shelf_file, G_in%Domain)) &
-        call MOM_error(FATAL, "MOM_initialize_state: Unable to open shelf file " &
-            // trim(ice_shelf_file))
+!      if (.not. file_exists(ice_shelf_file, G_in%Domain)) &
+!        call MOM_error(FATAL, "MOM_initialize_state: Unable to open shelf file " &
+!            // trim(ice_shelf_file))
 
-      allocate(area_shelf_in(G_in%isd:G_in%ied, G_in%jsd:G_in%jed))
-      call MOM_read_data(ice_shelf_file, trim(area_varname), area_shelf_in, &
-          G_in%Domain, scale=US%m_to_L**2)
+!      allocate(area_shelf_in(G_in%isd:G_in%ied, G_in%jsd:G_in%jed))
+!      call MOM_read_data(ice_shelf_file, trim(area_varname), area_shelf_in, &
+!          G_in%Domain, scale=US%m_to_L**2)
 
       ! Initialize frac_shelf_h with zeros (open water everywhere)
       allocate(frac_shelf_in(G_in%isd:G_in%ied, G_in%jsd:G_in%jed))
       frac_shelf_in(:,:) = 0.0
+      allocate(CS%frac_shelf_h(isd:ied, jsd:jed))
+      CS%frac_shelf_h(:,:) = 0.0
+      call ice_shelf_query(ice_shelf_CSp,G,CS%frac_shelf_h)
+      call rotate_array(CS%frac_shelf_h, -turns, frac_shelf_in)
+
+
 
       ! Compute fractional ice shelf coverage of h
-      do j = G_in%jsd, G_in%jed ; do i = G_in%isd, G_in%ied
-        if (G_in%areaT(i,j) > 0.0) &
-          frac_shelf_in(i,j) = area_shelf_in(i,j) / G_in%areaT(i,j)
-      enddo; enddo
+!      do j = G_in%jsd, G_in%jed ; do i = G_in%isd, G_in%ied
+!        if (G_in%areaT(i,j) > 0.0) &
+!          frac_shelf_in(i,j) = area_shelf_in(i,j) / G_in%areaT(i,j)
+!      enddo; enddo
       ! TODO: Verify that pass_var is needed here?
       call pass_var(frac_shelf_in, G_in%Domain)
-      deallocate(area_shelf_in)
+!      deallocate(area_shelf_in)
     endif
 
     if (use_ice_shelf) then
@@ -2433,14 +2446,14 @@ subroutine initialize_MOM(Time, Time_init, param_file, dirs, CS, restart_CSp, &
       call MOM_error(FATAL, "Index rotation of non-ALE sponge is not yet implemented.")
     endif
 
-    if (use_ice_shelf ) then
-      allocate(CS%frac_shelf_h(isd:ied, jsd:jed))
-      CS%frac_shelf_h(:,:) = 0.0
+!    if (use_ice_shelf ) then
+!      allocate(CS%frac_shelf_h(isd:ied, jsd:jed))
+!      CS%frac_shelf_h(:,:) = 0.0
 
-      call rotate_array(frac_shelf_in, turns, CS%frac_shelf_h)
+!      call rotate_array(frac_shelf_in, turns, CS%frac_shelf_h)
       ! TODO: Verify if pass_var is needed
-      call pass_var(CS%frac_shelf_h, G%Domain)
-    endif
+!      call pass_var(CS%frac_shelf_h, G%Domain)
+!    endif
 
     if (associated(ALE_sponge_in_CSp)) then
       call rotate_ALE_sponge(ALE_sponge_in_CSp, G_in, CS%ALE_sponge_CSp, G, turns, param_file)
@@ -2463,26 +2476,31 @@ subroutine initialize_MOM(Time, Time_init, param_file, dirs, CS, restart_CSp, &
   else
     if (use_ice_shelf) then
       ! TODO: Yes, this should be a function...
-      if (.not.file_exists(ice_shelf_file, G%Domain)) &
-        call MOM_error(FATAL, "MOM_initialize_state: Unable to open shelf file " &
-            // trim(ice_shelf_file))
+ !      if (.not.file_exists(ice_shelf_file, G%Domain)) &
+!         call MOM_error(FATAL, "MOM_initialize_state: Unable to open shelf file " &
+!             // trim(ice_shelf_file))
 
-      allocate(area_shelf_in(isd:ied, jsd:jed))
-      call MOM_read_data(ice_shelf_file, trim(area_varname), area_shelf_in, &
-          G%Domain, scale=US%m_to_L**2)
+!       allocate(area_shelf_in(isd:ied, jsd:jed))
+!       call MOM_read_data(ice_shelf_file, trim(area_varname), area_shelf_in, &
+!           G%Domain, scale=US%m_to_L**2)
 
-      ! Initialize frac_shelf_h with zeros (open water everywhere)
+!       ! Initialize frac_shelf_h with zeros (open water everywhere)
+!       allocate(CS%frac_shelf_h(isd:ied, jsd:jed))
+!       CS%frac_shelf_h(:,:) = 0.0
+
+!       ! Compute fractional ice shelf coverage of h
+!       do j = jsd, jed ; do i = isd, ied
+!         if (G%areaT(i,j) > 0.0) &
+!           CS%frac_shelf_h(i,j) = area_shelf_in(i,j) / G%areaT(i,j)
+!       enddo; enddo
+!       call pass_var(CS%frac_shelf_h,G%Domain)
+!       deallocate(area_shelf_in)
+! !
       allocate(CS%frac_shelf_h(isd:ied, jsd:jed))
       CS%frac_shelf_h(:,:) = 0.0
-
-      ! Compute fractional ice shelf coverage of h
-      do j = jsd, jed ; do i = isd, ied
-        if (G%areaT(i,j) > 0.0) &
-          CS%frac_shelf_h(i,j) = area_shelf_in(i,j) / G%areaT(i,j)
-      enddo; enddo
-      call pass_var(CS%frac_shelf_h,G%Domain)
-      deallocate(area_shelf_in)
-
+      call ice_shelf_query(ice_shelf_CSp,G,CS%frac_shelf_h)
+!
+!
       call MOM_initialize_state(CS%u, CS%v, CS%h, CS%tv, Time, G, GV, US, &
           param_file, dirs, restart_CSp, CS%ALE_CSp, CS%tracer_Reg, &
           CS%sponge_CSp, CS%ALE_sponge_CSp, CS%OBC, Time_in, &

commit cfc77cf90c408ebe0a384bc75ede95661f876fe7

Fixes ice shelf initialization bugs introduced in commit bb83604f2

config_src/solo_driver/MOM_driver.F90
@@ -28,6 +28,7 @@ program MOM_main
   use MOM_cpu_clock,       only : CLOCK_COMPONENT
   use MOM_diag_mediator,   only : enable_averaging, disable_averaging, diag_mediator_end
   use MOM_diag_mediator,   only : diag_ctrl, diag_mediator_close_registration
+  use MOM_IS_diag_mediator,   only : diag_IS_ctrl=>diag_ctrl, diag_mediator_IS_end=>diag_mediator_end
   use MOM,                 only : initialize_MOM, step_MOM, MOM_control_struct, MOM_end
   use MOM,                 only : extract_surface_state, finish_MOM_initialization
   use MOM,                 only : get_MOM_state_elements, MOM_state_is_synchronized
@@ -61,7 +62,7 @@ program MOM_main
   use MOM_verticalGrid,    only : verticalGrid_type
   use MOM_write_cputime,   only : write_cputime, MOM_write_cputime_init
   use MOM_write_cputime,   only : write_cputime_start_clock, write_cputime_CS
-
+  use MOM_get_input,       only : get_MOM_input
   use ensemble_manager_mod, only : ensemble_manager_init, get_ensemble_size
   use ensemble_manager_mod, only : ensemble_pelist_setup
   use mpp_mod, only : set_current_pelist => mpp_set_current_pelist
@@ -198,8 +199,10 @@ program MOM_main
   type(MOM_restart_CS),      pointer :: &
     restart_CSp => NULL()     !< A pointer to the restart control structure
                               !! that will be used for MOM restart files.
-  type(diag_ctrl), pointer :: &
-    diag => NULL()            !< A pointer to the diagnostic regulatory structure
+  type(diag_ctrl),           pointer :: &
+       diag => NULL()         !< A pointer to the diagnostic regulatory structure
+  type(diag_IS_ctrl), pointer :: &
+      diag_IS => NULL()       !< A pointer to the diagnostic regulatory structure
   !-----------------------------------------------------------------------
 
   character(len=4), parameter :: vers_num = 'v2.0'
@@ -305,14 +308,19 @@ program MOM_main
     Time = Start_time
   endif
 
+  ! Read paths and filenames from namelist and store in "dirs".
+  ! Also open the parsed input parameter file(s) and setup param_file.
+  call get_MOM_input(param_file, dirs)
+
   call get_param(param_file, mod_name, "ICE_SHELF", use_ice_shelf, &
-                 "If true, enables the ice shelf model.", default=.false.)
+       "If true, enables the ice shelf model.", default=.false.)
   if (use_ice_shelf) then
     ! These arrays are not initialized in most solo cases, but are needed
     ! when using an ice shelf
     call initialize_ice_shelf(param_file, grid, Time, ice_shelf_CSp, &
-                              diag, forces, fluxes, sfc_state)
+                              diag_IS, forces, fluxes, sfc_state)
   endif
+  call close_param_file(param_file)
 
   if (sum(date) >= 0) then
     call initialize_MOM(Time, Start_time, param_file, dirs, MOM_CSp, restart_CSp, &
@@ -440,6 +448,7 @@ program MOM_main
   ! Close the param_file.  No further parsing of input is possible after this.
   call close_param_file(param_file)
   call diag_mediator_close_registration(diag)
+
 
   ! Write out a time stamp file.
   if (calendar_type /= NO_CALENDAR) then
@@ -659,6 +668,9 @@ program MOM_main
 
   call callTree_waypoint("End MOM_main")
   call diag_mediator_end(Time, diag, end_diag_manager=.true.)
+  if (use_ice_shelf) then
+     call diag_mediator_IS_end(Time, diag_IS)
+  endif
   if (cpu_steps > 0) call write_cputime(Time, ns-1, write_CPU_CSp, call_end=.true.)
   call cpu_clock_end(termClock)
 

src/ice_shelf/MOM_ice_shelf.F90
@@ -9,9 +9,11 @@ use MOM_constants, only : hlf
 use MOM_cpu_clock, only : cpu_clock_id, cpu_clock_begin, cpu_clock_end
 use MOM_cpu_clock, only : CLOCK_COMPONENT, CLOCK_ROUTINE
 use MOM_coms,                 only : num_PEs
-use MOM_diag_mediator, only : post_data, register_diag_field, safe_alloc_ptr
-use MOM_diag_mediator, only : diag_mediator_init, set_diag_mediator_grid, diag_ctrl, time_type
-use MOM_diag_mediator, only : enable_averages, enable_averaging, disable_averaging
+use MOM_IS_diag_mediator, only : post_data, register_diag_field=>register_MOM_IS_diag_field, safe_alloc_ptr
+use MOM_IS_diag_mediator, only : set_axes_info
+use MOM_IS_diag_mediator, only : diag_mediator_init, set_diag_mediator_grid, diag_ctrl, time_type
+use MOM_IS_diag_mediator, only : enable_averages, enable_averaging, disable_averaging
+use MOM_IS_diag_mediator, only : diag_mediator_infrastructure_init, diag_mediator_close_registration
 use MOM_domains, only : MOM_domains_init, clone_MOM_domain
 use MOM_domains, only : pass_var, pass_vector, TO_ALL, CGRID_NE, BGRID_NE, CORNER
 use MOM_dyn_horgrid, only : dyn_horgrid_type, create_dyn_horgrid, destroy_dyn_horgrid
@@ -1146,7 +1148,7 @@ subroutine initialize_ice_shelf(param_file, ocn_grid, Time, CS, diag, forces_in,
   type(ocean_grid_type),        pointer       :: ocn_grid   !< The calling ocean model's horizontal grid structure
   type(time_type),              intent(inout) :: Time !< The clock that that will indicate the model time
   type(ice_shelf_CS),           pointer       :: CS   !< A pointer to the ice shelf control structure
-  type(diag_ctrl),    target,   intent(in)    :: diag !< A structure that is used to regulate the diagnostic output.
+  type(diag_ctrl),              pointer       :: diag !< A structure that is used to regulate the diagnostic output.
   type(mech_forcing), optional, pointer       :: forces_in !< A structure with the driving mechanical forces
   type(forcing),      optional, pointer       :: fluxes_in !< A structure containing pointers to any possible
                                                            !! thermodynamic or mass-flux forcing fields.
@@ -1208,6 +1210,8 @@ subroutine initialize_ice_shelf(param_file, ocn_grid, Time, CS, diag, forces_in,
   ! MOM's grid and infrastructure.
   call Get_MOM_Input(dirs=dirs)
 
+  call diag_mediator_infrastructure_init()
+
   ! Determining the internal unit scaling factors for this run.
   call unit_scaling_init(param_file, CS%US)
 
@@ -1254,7 +1258,6 @@ subroutine initialize_ice_shelf(param_file, ocn_grid, Time, CS, diag, forces_in,
     call rotate_dyngrid(dG_in, dG, CS%US, CS%turns)
     call copy_dyngrid_to_MOM_grid(dG,CS%Grid,CS%US)
   else
-    ! call diag_mediator_init(CS%grid,param_file,CS%diag)
      ! this needs to be fixed - will probably break when not using coupled driver 0
     CS%Grid=>CS%Grid_in
     CS%HI=>CS%HI_in
@@ -1268,6 +1271,12 @@ subroutine initialize_ice_shelf(param_file, ocn_grid, Time, CS, diag, forces_in,
   endif
   G=>CS%Grid
 
+  allocate(diag)
+  call diag_mediator_init(G, param_file,diag,component='MOM_IceShelf')
+  ! This call sets up the diagnostic axes. These are needed,
+  ! e.g. to generate the target grids below.
+  call set_axes_info(G, param_file, diag)
+
 
   is = G%isc ; ie = G%iec ; js = G%jsc ; je = G%jec
   isd = G%isd ; jsd = G%jsd ; ied = G%ied ; jed = G%jed
@@ -1280,7 +1289,7 @@ subroutine initialize_ice_shelf(param_file, ocn_grid, Time, CS, diag, forces_in,
   ! Convenience pointers
   OG => CS%ocn_grid
   US => CS%US
-  CS%diag => diag
+  CS%diag=>diag
 
   ! Are we being called from the solo ice-sheet driver? When called by the ocean
   ! model solo_ice_sheet_in is not preset.
@@ -1779,13 +1788,13 @@ subroutine initialize_ice_shelf(param_file, ocn_grid, Time, CS, diag, forces_in,
   endif
 
 
-  CS%id_area_shelf_h = register_diag_field('ocean_model', 'area_shelf_h', CS%diag%axesT1, CS%Time, &
+  CS%id_area_shelf_h = register_diag_field('ice_shelf_model', 'area_shelf_h', CS%diag%axesT1, CS%Time, &
      'Ice Shelf Area in cell', 'meter-2', conversion=US%L_to_m**2)
-  CS%id_shelf_mass = register_diag_field('ocean_model', 'shelf_mass', CS%diag%axesT1, CS%Time, &
+  CS%id_shelf_mass = register_diag_field('ice_shelf_model', 'shelf_mass', CS%diag%axesT1, CS%Time, &
      'mass of shelf', 'kg/m^2', conversion=US%RZ_to_kg_m2)
-  CS%id_h_shelf = register_diag_field('ocean_model', 'h_shelf', CS%diag%axesT1, CS%Time, &
+  CS%id_h_shelf = register_diag_field('ice_shelf_model', 'h_shelf', CS%diag%axesT1, CS%Time, &
        'ice shelf thickness', 'm', conversion=US%Z_to_m)
-  CS%id_mass_flux = register_diag_field('ocean_model', 'mass_flux', CS%diag%axesT1,&
+  CS%id_mass_flux = register_diag_field('ice_shelf_model', 'mass_flux', CS%diag%axesT1,&
      CS%Time, 'Total mass flux of freshwater across the ice-ocean interface.', &
      'kg/s', conversion=US%RZ_T_to_kg_m2s*US%L_to_m**2)
 
@@ -1794,32 +1803,34 @@ subroutine initialize_ice_shelf(param_file, ocn_grid, Time, CS, diag, forces_in,
   else ! use original eq.
     meltrate_conversion = 86400.0*365.0*US%Z_to_m*US%s_to_T / CS%density_ice
   endif
-  CS%id_melt = register_diag_field('ocean_model', 'melt', CS%diag%axesT1, CS%Time, &
+  CS%id_melt = register_diag_field('ice_shelf_model', 'melt', CS%diag%axesT1, CS%Time, &
      'Ice Shelf Melt Rate', 'm yr-1', conversion= meltrate_conversion)
-  CS%id_thermal_driving = register_diag_field('ocean_model', 'thermal_driving', CS%diag%axesT1, CS%Time, &
+  CS%id_thermal_driving = register_diag_field('ice_shelf_model', 'thermal_driving', CS%diag%axesT1, CS%Time, &
      'pot. temp. in the boundary layer minus freezing pot. temp. at the ice-ocean interface.', 'Celsius')
-  CS%id_haline_driving = register_diag_field('ocean_model', 'haline_driving', CS%diag%axesT1, CS%Time, &
+  CS%id_haline_driving = register_diag_field('ice_shelf_model', 'haline_driving', CS%diag%axesT1, CS%Time, &
      'salinity in the boundary layer minus salinity at the ice-ocean interface.', 'psu')
-  CS%id_Sbdry = register_diag_field('ocean_model', 'sbdry', CS%diag%axesT1, CS%Time, &
+  CS%id_Sbdry = register_diag_field('ice_shelf_model', 'sbdry', CS%diag%axesT1, CS%Time, &
      'salinity at the ice-ocean interface.', 'psu')
-  CS%id_u_ml = register_diag_field('ocean_model', 'u_ml', CS%diag%axesCu1, CS%Time, &
+  CS%id_u_ml = register_diag_field('ice_shelf_model', 'u_ml', CS%diag%axesCu1, CS%Time, &
      'Eastward vel. in the boundary layer (used to compute ustar)', 'm s-1', conversion=US%L_T_to_m_s)
-  CS%id_v_ml = register_diag_field('ocean_model', 'v_ml', CS%diag%axesCv1, CS%Time, &
+  CS%id_v_ml = register_diag_field('ice_shelf_model', 'v_ml', CS%diag%axesCv1, CS%Time, &
      'Northward vel. in the boundary layer (used to compute ustar)', 'm s-1', conversion=US%L_T_to_m_s)
-  CS%id_exch_vel_s = register_diag_field('ocean_model', 'exch_vel_s', CS%diag%axesT1, CS%Time, &
+  CS%id_exch_vel_s = register_diag_field('ice_shelf_model', 'exch_vel_s', CS%diag%axesT1, CS%Time, &
      'Sub-shelf salinity exchange velocity', 'm s-1', conversion=US%Z_to_m*US%s_to_T)
-  CS%id_exch_vel_t = register_diag_field('ocean_model', 'exch_vel_t', CS%diag%axesT1, CS%Time, &
+  CS%id_exch_vel_t = register_diag_field('ice_shelf_model', 'exch_vel_t', CS%diag%axesT1, CS%Time, &
      'Sub-shelf thermal exchange velocity', 'm s-1' , conversion=US%Z_to_m*US%s_to_T)
-  CS%id_tfreeze = register_diag_field('ocean_model', 'tfreeze', CS%diag%axesT1, CS%Time, &
+  CS%id_tfreeze = register_diag_field('ice_shelf_model', 'tfreeze', CS%diag%axesT1, CS%Time, &
      'In Situ Freezing point at ice shelf interface', 'degC')
-  CS%id_tfl_shelf = register_diag_field('ocean_model', 'tflux_shelf', CS%diag%axesT1, CS%Time, &
+  CS%id_tfl_shelf = register_diag_field('ice_shelf_model', 'tflux_shelf', CS%diag%axesT1, CS%Time, &
      'Heat conduction into ice shelf', 'W m-2', conversion=-US%QRZ_T_to_W_m2)
-  CS%id_ustar_shelf = register_diag_field('ocean_model', 'ustar_shelf', CS%diag%axesT1, CS%Time, &
+  CS%id_ustar_shelf = register_diag_field('ice_shelf_model', 'ustar_shelf', CS%diag%axesT1, CS%Time, &
      'Fric vel under shelf', 'm/s', conversion=US%Z_to_m*US%s_to_T)
   if (CS%active_shelf_dynamics) then
-    CS%id_h_mask = register_diag_field('ocean_model', 'h_mask', CS%diag%axesT1, CS%Time, &
+    CS%id_h_mask = register_diag_field('ice_shelf_model', 'h_mask', CS%diag%axesT1, CS%Time, &
        'ice shelf thickness mask', 'none')
   endif
+  call diag_mediator_close_registration(CS%diag)
+
 
   if (present(fluxes_in) .and. CS%rotate_index) &
      call rotate_forcing(fluxes, fluxes_in, -CS%turns)

src/ice_shelf/MOM_ice_shelf_dynamics.F90
@@ -6,9 +6,9 @@ module MOM_ice_shelf_dynamics
 
 use MOM_cpu_clock, only : cpu_clock_id, cpu_clock_begin, cpu_clock_end
 use MOM_cpu_clock, only : CLOCK_COMPONENT, CLOCK_ROUTINE
-use MOM_diag_mediator, only : post_data, register_diag_field, safe_alloc_ptr
-use MOM_diag_mediator, only : diag_mediator_init, set_diag_mediator_grid
-use MOM_diag_mediator, only : diag_ctrl, time_type, enable_averages, disable_averaging
+use MOM_IS_diag_mediator, only : post_data, register_diag_field=>register_MOM_IS_diag_field, safe_alloc_ptr
+use MOM_IS_diag_mediator, only : diag_mediator_init, set_diag_mediator_grid
+use MOM_IS_diag_mediator, only : diag_ctrl, time_type, enable_averages, disable_averaging
 use MOM_domains, only : MOM_domains_init, clone_MOM_domain
 use MOM_domains, only : pass_var, pass_vector, TO_ALL, CGRID_NE, BGRID_NE, CORNER
 use MOM_error_handler, only : MOM_error, MOM_mesg, FATAL, WARNING, is_root_pe