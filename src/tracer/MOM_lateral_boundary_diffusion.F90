87ba533e42ad473a6a069efb59a7af7691015754
Gustavo Marques <gmarques@ucar.edu>
2020-10-15 11:39:40 -0600

    Adds missing args for KPP_get_BLD and energetic_PBL_get_MLD

--- src/tracer/MOM_lateral_boundary_diffusion.F90
+++ src/tracer/MOM_lateral_boundary_diffusion.F90
@@ -182,8 +182,9 @@ subroutine lateral_boundary_diffusion(G, GV, US, h, Coef_x, Coef_y, dt, Reg, CS)
   real    :: tmpReal, tmp1, tmp2
 
   Idt = 1./dt
-  if (ASSOCIATED(CS%KPP_CSp)) call KPP_get_BLD(CS%KPP_CSp, hbl, G)
-  if (ASSOCIATED(CS%energetic_PBL_CSp)) call energetic_PBL_get_MLD(CS%energetic_PBL_CSp, hbl, G, US)
+  if (ASSOCIATED(CS%KPP_CSp)) call KPP_get_BLD(CS%KPP_CSp, hbl, G, US, m_to_BLD_units=GV%m_to_H)
+  if (ASSOCIATED(CS%energetic_PBL_CSp)) call energetic_PBL_get_MLD(CS%energetic_PBL_CSp, hbl, G, US, &
+                                                                   m_to_MLD_units=GV%m_to_H)
 
   call pass_var(hbl,G%Domain)
   do m = 1,Reg%ntr
