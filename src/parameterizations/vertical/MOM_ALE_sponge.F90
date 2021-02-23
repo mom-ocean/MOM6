ed46c3cc085c6781d73b94ec4e1932ad24c5f22c
Matthew Harrison <Matthew.Harrison@noaa.gov>
2021-02-23 13:31:36 -0500

    *Bugfix for ALE sponges (pass non-zero target thickness to remapping)
    	- this addresses a bug in the apply_ALE_sponge routine which
      	had been passing zero values to the remapping routine for the
    	target grid thicknesses.

--- src/parameterizations/vertical/MOM_ALE_sponge.F90
+++ src/parameterizations/vertical/MOM_ALE_sponge.F90
@@ -961,11 +961,14 @@ subroutine apply_ALE_sponge(h, dt, G, GV, US, CS, Time)
       I1pdamp = 1.0 / (1.0 + damp)
       tmp_val2(1:nz_data) = CS%Ref_val(m)%p(1:nz_data,c)
       if (CS%time_varying_sponges) then
+
         call remapping_core_h(CS%remap_cs, nz_data, CS%Ref_val(m)%h(1:nz_data,c), tmp_val2, &
-                              CS%nz, h_col, tmp_val1, h_neglect, h_neglect_edge)
+             CS%nz, h(i,j,:), tmp_val1, h_neglect, h_neglect_edge)
+         print *,'calling remapping_core_h',h(i,j,:),tmp_val2(:),tmp_val1(:)
       else
         call remapping_core_h(CS%remap_cs, nz_data, CS%Ref_h%p(1:nz_data,c), tmp_val2, &
-                              CS%nz, h_col, tmp_val1, h_neglect, h_neglect_edge)
+             CS%nz, h(i,j,:), tmp_val1, h_neglect, h_neglect_edge)
+         print *,'Calling remapping_core_h',h(i,j,1),tmp_val1(1)
       endif
       !Backward Euler method
       if (CS%id_sp_tendency(m) > 0) tmp(i,j,1:nz) = CS%var(m)%p(i,j,1:nz)
