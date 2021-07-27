5b2040ededd96d8104934fd15bacde38a10b118c
pjpegion <Philip.Pegion@noaa.gov>
2021-07-27 17:08:13 +0000

    add logic to remove incrments from restart if outside IAU window

--- src/ocean_data_assim/MOM_oda_incupd.F90
+++ src/ocean_data_assim/MOM_oda_incupd.F90
@@ -588,6 +588,16 @@ subroutine apply_oda_incupd(h, tv, u, v, dt, G, GV, US, CS)
   if (CS%ncount >= CS%nstep_incupd) then
     if (is_root_pe()) call MOM_error(NOTE,"ended updating fields with increments. ")
     return
+    if (CS%ncount == CS%nstep_incupd) then
+       call register_restart_field_as_obsolete("oda_incupd_ncount", "none", CS)
+       call register_restart_field_as_obsolete("T_inc", "none", CS)
+       call register_restart_field_as_obsolete("S_inc", "none", CS)
+       call register_restart_field_as_obsolete("h_obs", "none", CS)
+       if (CS%uv_inc) then
+          call register_restart_field_as_obsolete("u_inc", "none", CS)
+          call register_restart_field_as_obsolete("v_inc", "none", CS)
+       endif
+     endif
   endif !ncount>CS%nstep_incupd
 
   ! update counter
