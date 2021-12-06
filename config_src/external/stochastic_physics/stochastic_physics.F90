38b1e96950928b0cef60d27ce3f604205ea0043f
pjpegion <Philip.Pegion@noaa.gov>
2021-12-06 23:08:23 +0000

    add comments to stochastic physics stub

--- config_src/external/stochastic_physics/stochastic_physics.F90
+++ config_src/external/stochastic_physics/stochastic_physics.F90
@@ -16,12 +16,17 @@ contains
 subroutine init_stochastic_physics_ocn(delt,geoLonT,geoLatT,nx,ny,nz,pert_epbl_in,do_sppt_in, &
                                        mpiroot, mpicomm, iret)
 implicit none
-real,intent(in)  :: delt
-integer,intent(in) :: nx,ny,nz
-real,intent(in) :: geoLonT(nx,ny),geoLatT(nx,ny)
-logical,intent(in) :: pert_epbl_in,do_sppt_in
-integer,intent(in)    :: mpiroot, mpicomm
-integer, intent(out) :: iret
+real,intent(in)       :: delt !< timestep in seconds between calls to run_stochastic_physics_ocn
+integer,intent(in)    :: nx   !< number of gridpoints in the x-direction of the compute grid
+integer,intent(in)    :: ny   !< number of gridpoints in the y-direction of the compute grid
+integer,intent(in)    :: nz   !< number of gridpoints in the z-direction of the compute grid
+real,intent(in)       :: geoLonT(nx,ny) !< Longitude in degrees
+real,intent(in)       :: geoLatT(nx,ny) !< Latitude in degrees
+logical,intent(in)    :: pert_epbl_in !< logical flag, if true generate random pattern for ePBL perturbations
+logical,intent(in)    :: do_sppt_in   !< logical flag, if true generate random pattern for SPPT perturbations
+integer,intent(in)    :: mpiroot !< root processor
+integer,intent(in)    :: mpicomm !< mpi communicator
+integer, intent(out)  :: iret    !< return code
 
 iret=0
 if (pert_epbl_in .EQV. .true. ) then
@@ -37,7 +42,9 @@ end subroutine init_stochastic_physics_ocn
 
 subroutine run_stochastic_physics_ocn(sppt_wts,t_rp1,t_rp2)
 implicit none
-real, intent(inout) :: sppt_wts(:,:),t_rp1(:,:),t_rp2(:,:)
+real, intent(inout) :: sppt_wts(:,:)  !< array contain random weight for SPPT range [0,2]
+real, intent(inout) :: t_rp1(:,:)  !< array containing random weights for ePBL perturbations (KE generation) range [0,2]
+real, intent(inout) :: t_rp2(:,:)  !< array containing random weights for ePBL perturbations (KE dissipation) range [0,2]
 return
 end subroutine run_stochastic_physics_ocn
 
@@ -52,7 +59,7 @@ public  :: write_stoch_restart_ocn
 contains
 subroutine write_stoch_restart_ocn(sfile)
 
-character(len=*) :: sfile
+character(len=*) :: sfile   !< name of restart file
 return
 end subroutine write_stoch_restart_ocn
 
