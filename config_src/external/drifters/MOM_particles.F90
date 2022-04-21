1d8fb50917c8afc0284e3fd0846d8bbaef942556
Robert Hallberg <Robert.Hallberg@noaa.gov>
2022-04-21 12:27:16 -0400

    +Style compliance in external/drifters code
      Revised the external/drifters code to bring it into closer alignment with the
    MOM6 style guide at https://github.com/mom-ocean/MOM6/wiki/Code-style-guide.
    This includes using standard syntax to document variable units, the addition of
    'implicit none ; private', explicit public statements, and a licensing statement
    to the files that were missing one, and modifications to follow the MOM6 2-point
    indenting convention.  All answers are bitwise identical.

--- config_src/external/drifters/MOM_particles.F90
+++ config_src/external/drifters/MOM_particles.F90
@@ -1,26 +1,28 @@
 !> A set of dummy interfaces for compiling the MOM6 drifters code
 module MOM_particles_mod
 
+! This file is part of MOM6. See LICENSE.md for the license.
+
 use MOM_grid,         only : ocean_grid_type
 use MOM_time_manager, only : time_type, get_date, operator(-)
 use MOM_variables,    only : thermo_var_ptrs
+use particles_types_mod, only : particles, particles_gridded
 
+implicit none ; private
 
-use particles_types_mod, only: particles, particles_gridded
-
-public particles_run, particles_init, particles_save_restart, particles_end
+public particles, particles_run, particles_init, particles_save_restart, particles_end
 
 contains
 
 !> Initializes particles container "parts"
 subroutine particles_init(parts, Grid, Time, dt, u, v)
   ! Arguments
- type(particles), pointer, intent(out) :: parts !< Container for all types and memory
- type(ocean_grid_type), target, intent(in) :: Grid !< Grid type from parent model
- type(time_type), intent(in) :: Time !< Time type from parent model
- real, intent(in)            :: dt !< particle timestep in seconds
- real, dimension(:,:,:),intent(in)      :: u !< Zonal velocity field
- real, dimension(:,:,:),intent(in)      :: v !< Meridional velocity field
+  type(particles), pointer, intent(out) :: parts !< Container for all types and memory
+  type(ocean_grid_type), target, intent(in) :: Grid !< Grid type from parent model
+  type(time_type), intent(in) :: Time !< Time type from parent model
+  real, intent(in)            :: dt   !< particle timestep [s]
+  real, dimension(:,:,:), intent(in)    :: u !< Zonal velocity field [m s-1]
+  real, dimension(:,:,:), intent(in)    :: v !< Meridional velocity field [m s-1]
 
 end subroutine particles_init
 
@@ -29,30 +31,30 @@ subroutine particles_run(parts, time, uo, vo, ho, tv, stagger)
   ! Arguments
   type(particles), pointer :: parts !< Container for all types and memory
   type(time_type), intent(in) :: time !< Model time
-  real, dimension(:,:,:),intent(in) :: uo !< Ocean zonal velocity (m/s)
-  real, dimension(:,:,:),intent(in) :: vo !< Ocean meridional velocity (m/s)
-  real, dimension(:,:,:),intent(in) :: ho !< Ocean layer thickness [H ~> m or kg m-2]
-  type(thermo_var_ptrs), intent(in) :: tv !< structure containing pointers to available thermodynamic fields
+  real, dimension(:,:,:), intent(in) :: uo !< Ocean zonal velocity [m s-1]
+  real, dimension(:,:,:), intent(in) :: vo !< Ocean meridional velocity [m s-1]
+  real, dimension(:,:,:), intent(in) :: ho !< Ocean layer thickness [H ~> m or kg m-2]
+  type(thermo_var_ptrs),  intent(in) :: tv !< structure containing pointers to available thermodynamic fields
   integer, optional, intent(in) :: stagger !< Flag for whether velocities are staggered
 
 end subroutine particles_run
 
 
 !>Save particle locations (and sometimes other vars) to restart file
-subroutine particles_save_restart(parts,temp,salt)
-! Arguments
-type(particles), pointer :: parts !< Container for all types and memory
-real,dimension(:,:,:),optional,intent(in) :: temp !< Optional container for temperature
-real,dimension(:,:,:),optional,intent(in) ::  salt !< Optional container for salinity
+subroutine particles_save_restart(parts, temp, salt)
+  ! Arguments
+  type(particles), pointer :: parts !< Container for all types and memory
+  real, dimension(:,:,:), optional, intent(in) :: temp !< Optional container for temperature
+  real, dimension(:,:,:), optional, intent(in) :: salt !< Optional container for salinity
 
 end subroutine particles_save_restart
 
 !> Deallocate all memory and disassociated pointer
-subroutine particles_end(parts,temp,salt)
-! Arguments
-type(particles), pointer :: parts !< Container for all types and memory
-real,dimension(:,:,:),optional,intent(in) :: temp !< Optional container for temperature
-real,dimension(:,:,:),optional,intent(in) :: salt !< Optional container for salinity
+subroutine particles_end(parts, temp, salt)
+  ! Arguments
+  type(particles), pointer :: parts !< Container for all types and memory
+  real, dimension(:,:,:), optional, intent(in) :: temp !< Optional container for temperature
+  real, dimension(:,:,:), optional, intent(in) :: salt !< Optional container for salinity
 
 end subroutine particles_end
 
