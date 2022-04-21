84d78a661b1d57eef7f6f98119bb055138fded6c
Robert Hallberg <Robert.Hallberg@noaa.gov>
2022-04-21 12:26:29 -0400

    +Revised stochastics code for style compliance
      Revised the stochastics-related code to bring it into closer alignment with
    the MOM6 style guide at https://github.com/mom-ocean/MOM6/wiki/Code-style-guide,
    and splitting config_src/external/stochastic_physics/stochastic_physics.F90 into
    two files (one of which is the new file get_stochy_pattern.F90) to respect the
    MOM6 convention that there is only one module per file.  Also renamed two
    stochastics-related optional arguments to ePBL_column to better reflect what
    they do, and corrected a number of spelling errors in comments in the files that
    were being modified.  All answers and output are bitwise identical, but there
    are some minor internal interface changes.

--- config_src/external/stochastic_physics/stochastic_physics.F90
+++ config_src/external/stochastic_physics/stochastic_physics.F90
@@ -3,66 +3,56 @@
 ! http://github.com/noaa-psd/stochastic_physics
 module stochastic_physics
 
-implicit none
+! This file is part of MOM6. See LICENSE.md for the license.
 
-private
+use MOM_error_handler, only : MOM_error, WARNING
+
+implicit none ; private
 
 public :: init_stochastic_physics_ocn
 public :: run_stochastic_physics_ocn
 
 contains
 
-!!!!!!!!!!!!!!!!!!!!
-subroutine init_stochastic_physics_ocn(delt,geoLonT,geoLatT,nx,ny,nz,pert_epbl_in,do_sppt_in, &
+!> Initializes the stochastic physics perturbations.
+subroutine init_stochastic_physics_ocn(delt, geoLonT, geoLatT, nx, ny, nz, pert_epbl_in, do_sppt_in, &
                                        mpiroot, mpicomm, iret)
-implicit none
-real,intent(in)       :: delt !< timestep in seconds between calls to run_stochastic_physics_ocn
-integer,intent(in)    :: nx   !< number of gridpoints in the x-direction of the compute grid
-integer,intent(in)    :: ny   !< number of gridpoints in the y-direction of the compute grid
-integer,intent(in)    :: nz   !< number of gridpoints in the z-direction of the compute grid
-real,intent(in)       :: geoLonT(nx,ny) !< Longitude in degrees
-real,intent(in)       :: geoLatT(nx,ny) !< Latitude in degrees
-logical,intent(in)    :: pert_epbl_in !< logical flag, if true generate random pattern for ePBL perturbations
-logical,intent(in)    :: do_sppt_in   !< logical flag, if true generate random pattern for SPPT perturbations
-integer,intent(in)    :: mpiroot !< root processor
-integer,intent(in)    :: mpicomm !< mpi communicator
-integer, intent(out)  :: iret    !< return code
+  real,    intent(in)    :: delt !< timestep in seconds between calls to run_stochastic_physics_ocn [s]
+  integer, intent(in)    :: nx   !< number of gridpoints in the x-direction of the compute grid
+  integer, intent(in)    :: ny   !< number of gridpoints in the y-direction of the compute grid
+  integer, intent(in)    :: nz   !< number of gridpoints in the z-direction of the compute grid
+  real,    intent(in)    :: geoLonT(nx,ny) !< Longitude in degrees
+  real,    intent(in)    :: geoLatT(nx,ny) !< Latitude in degrees
+  logical, intent(in)    :: pert_epbl_in !< logical flag, if true generate random pattern for ePBL perturbations
+  logical, intent(in)    :: do_sppt_in   !< logical flag, if true generate random pattern for SPPT perturbations
+  integer, intent(in)    :: mpiroot !< root processor
+  integer, intent(in)    :: mpicomm !< mpi communicator
+  integer, intent(out)   :: iret    !< return code
 
-iret=0
-if (pert_epbl_in .EQV. .true. ) then
-   print*,'pert_epbl needs to be false if using the stub'
-   iret=-1
-endif
-if (do_sppt_in.EQV. .true. ) then
-   print*,'do_sppt needs to be false if using the stub'
-   iret=-1
-endif
-return
+  iret=0
+  if (pert_epbl_in) then
+    call MOM_error(WARNING, 'init_stochastic_physics_ocn: pert_epbl needs to be false if using the stub')
+    iret=-1
+  endif
+  if (do_sppt_in) then
+    call MOM_error(WARNING, 'init_stochastic_physics_ocn: do_sppt needs to be false if using the stub')
+    iret=-1
+  endif
+
+  ! This stub function does not actually do anything.
+  return
 end subroutine init_stochastic_physics_ocn
 
-subroutine run_stochastic_physics_ocn(sppt_wts,t_rp1,t_rp2)
-implicit none
-real, intent(inout) :: sppt_wts(:,:) !< array containing random weights for SPPT range [0,2]
-real, intent(inout) :: t_rp1(:,:)    !< array containing random weights for ePBL
-                                     !! perturbations (KE generation) range [0,2]
-real, intent(inout) :: t_rp2(:,:)    !< array containing random weights for ePBL
-                                     !! perturbations (KE dissipation) range [0,2]
-return
+!> Determines the stochastic physics perturbations.
+subroutine run_stochastic_physics_ocn(sppt_wts, t_rp1, t_rp2)
+  real, intent(inout) :: sppt_wts(:,:) !< array containing random weights for SPPT range [0,2]
+  real, intent(inout) :: t_rp1(:,:)    !< array containing random weights for ePBL
+                                       !! perturbations (KE generation) range [0,2]
+  real, intent(inout) :: t_rp2(:,:)    !< array containing random weights for ePBL
+                                       !! perturbations (KE dissipation) range [0,2]
+
+  ! This stub function does not actually do anything.
+  return
 end subroutine run_stochastic_physics_ocn
 
 end module stochastic_physics
-
-module get_stochy_pattern_mod
-
-private
-
-public  :: write_stoch_restart_ocn
-
-contains
-subroutine write_stoch_restart_ocn(sfile)
-
-character(len=*) :: sfile   !< name of restart file
-return
-end subroutine write_stoch_restart_ocn
-
-end module get_stochy_pattern_mod
