e7241fd5906cd5b31904ec803d7693cab2d1f4b3
Edward Yang <edward_yang_125@hotmail.com>
2025-03-12 14:30:35 +1100

    meridional_flux_adjust_fused: clean up var defs

--- src/core/MOM_continuity_PPM.F90
+++ src/core/MOM_continuity_PPM.F90
@@ -2133,42 +2133,44 @@ end subroutine meridional_flux_thickness
 subroutine meridional_flux_adjust_fused(v, h_in, h_S, h_N, vhbt, vh_tot_0, dvhdv_tot_0, &
                              dv, dv_max_CFL, dv_min_CFL, dt, G, GV, US, CS, visc_rem, &
                              ish, ieh, jsh, jeh, do_I_in, por_face_areaV, vh_3d, OBC)
-  type(ocean_grid_type),   intent(in)    :: G    !< Ocean's grid structure.
-  type(verticalGrid_type), intent(in)    :: GV   !< Ocean's vertical grid structure.
+  type(ocean_grid_type),             intent(in)  :: G    !< Ocean's grid structure.
+  type(verticalGrid_type),           intent(in)  :: GV   !< Ocean's vertical grid structure.
   real, dimension(SZI_(G),SZJB_(G),SZK_(GV)), &
-                           intent(in)    :: v    !< Meridional velocity [L T-1 ~> m s-1].
+                                     intent(in)  :: v    !< Meridional velocity [L T-1 ~> m s-1].
   real, dimension(SZI_(G),SZJ_(G),SZK_(GV)), &
-                           intent(in)    :: h_in !< Layer thickness used to calculate fluxes [H ~> m or kg m-2].
+                                     intent(in)  :: h_in !< Layer thickness used to calculate fluxes [H ~> m or kg m-2].
   real, dimension(SZI_(G),SZJ_(G),SZK_(GV)),&
-                           intent(in)    :: h_S  !< South edge thickness in the reconstruction [H ~> m or kg m-2].
+                                     intent(in)  :: h_S  !< South edge thickness in the reconstruction [H ~> m or kg m-2].
   real, dimension(SZI_(G),SZJ_(G),SZK_(GV)), &
-                           intent(in)    :: h_N  !< North edge thickness in the reconstruction [H ~> m or kg m-2].
-  real, dimension(SZI_(G),SZJB_(G),SZK_(GV)), intent(in) :: visc_rem
-                             !< Both the fraction of the momentum originally
+                                     intent(in)  :: h_N  !< North edge thickness in the reconstruction [H ~> m or kg m-2].
+  real, dimension(SZI_(G),SZJB_(G),SZK_(GV)), &
+                                     intent(in)  :: visc_rem !< Both the fraction of the momentum originally
                              !! in a layer that remains after a time-step of viscosity, and the
                              !! fraction of a time-step's worth of a barotropic acceleration that
                              !! a layer experiences after viscosity is applied [nondim].
                              !! Visc_rem is between 0 (at the bottom) and 1 (far above the bottom).
-  real, dimension(SZI_(G),SZJB_(G)), intent(in)    :: vhbt !< The summed volume flux through meridional faces
-                                                  !! [H L2 T-1 ~> m3 s-1 or kg s-1].
-  real, dimension(SZI_(G),SZJB_(G)), intent(in)    :: dv_max_CFL !< Maximum acceptable value of dv [L T-1 ~> m s-1].
-  real, dimension(SZI_(G),SZJB_(G)), intent(in)    :: dv_min_CFL !< Minimum acceptable value of dv [L T-1 ~> m s-1].
-  real, dimension(SZI_(G),SZJB_(G)), intent(in)    :: vh_tot_0   !< The summed transport with 0 adjustment
-                                                  !! [H L2 T-1 ~> m3 s-1 or kg s-1].
-  real, dimension(SZI_(G),SZJB_(G)), intent(in)    :: dvhdv_tot_0 !< The partial derivative of dv_err with
-                                                  !! dv at 0 adjustment [H L ~> m2 or kg m-1].
-  real, dimension(SZI_(G),SZJB_(G)), intent(out)   :: dv   !< The barotropic velocity adjustment [L T-1 ~> m s-1].
-  real,                     intent(in)    :: dt   !< Time increment [T ~> s].
-  type(unit_scale_type),    intent(in)    :: US   !< A dimensional unit scaling type
-  type(continuity_PPM_CS),  intent(in)    :: CS   !< This module's control structure.
-  integer,                  intent(in)    :: ish, jsh  !< Start of index range.
-  integer,                  intent(in)    :: ieh, jeh  !< End of index range.
+  real, dimension(SZI_(G),SZJB_(G)), intent(in)  :: vhbt !< The summed volume flux through meridional faces
+                                                         !! [H L2 T-1 ~> m3 s-1 or kg s-1].
+  real, dimension(SZI_(G),SZJB_(G)), intent(in)  :: dv_max_CFL !< Maximum acceptable value of dv [L T-1 ~> m s-1].
+  real, dimension(SZI_(G),SZJB_(G)), intent(in)  :: dv_min_CFL !< Minimum acceptable value of dv [L T-1 ~> m s-1].
+  real, dimension(SZI_(G),SZJB_(G)), intent(in)  :: vh_tot_0   !< The summed transport with 0 adjustment
+                                                               !! [H L2 T-1 ~> m3 s-1 or kg s-1].
+  real, dimension(SZI_(G),SZJB_(G)), intent(in)  :: dvhdv_tot_0 !< The partial derivative of dv_err with
+                                                                !! dv at 0 adjustment [H L ~> m2 or kg m-1].
+  real, dimension(SZI_(G),SZJB_(G)), intent(out) :: dv         !< The barotropic velocity adjustment [L T-1 ~> m s-1].
+  real,                              intent(in)  :: dt         !< Time increment [T ~> s].
+  type(unit_scale_type),             intent(in)  :: US         !< A dimensional unit scaling type
+  type(continuity_PPM_CS),           intent(in)  :: CS         !< This module's control structure.
+  integer,                           intent(in)  :: ish        !< Start of i index range.
+  integer,                           intent(in)  :: ieh        !< End of i index range.
+  integer,                           intent(in)  :: jsh        !< Start of j index range.
+  integer,                           intent(in)  :: jeh        !< End of j index range.
   logical, dimension(SZI_(G),SZJB_(G)), &
-                            intent(in)    :: do_I_in  !< A flag indicating which I values to work on.
+                                     intent(in)  :: do_I_in  !< A flag indicating which I values to work on.
   real, dimension(SZI_(G),SZJB_(G),SZK_(G)), &
-                     intent(in) :: por_face_areaV !< fractional open area of V-faces [nondim]
+                                     intent(in)  :: por_face_areaV !< fractional open area of V-faces [nondim]
   real, dimension(SZI_(G),SZJB_(G),SZK_(GV)), &
-                  optional, intent(inout) :: vh_3d !< Volume flux through meridional
+                         optional, intent(inout) :: vh_3d !< Volume flux through meridional
                              !! faces = v*h*dx [H L2 T-1 ~> m3 s-1 or kg s-1].
   type(ocean_OBC_type), optional, pointer :: OBC !< Open boundaries control structure.
   ! Local variables
