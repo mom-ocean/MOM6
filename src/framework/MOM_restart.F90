fa283a338e46d92836b7610a6510fb19087e794d
minghang.li <24727729+minghangli-uni@users.noreply.github.com>
2025-08-19 13:53:42 +1000

    Support restarting from decomposed restart files when restart filenames are provided (#23)
    Prior to this commit, restarting from decomposed restart files is only supported when reading automatically named files (filename='r'). This commit extends support to cases when restart filenames are explicitly provided.
    
    Co-authored-by: Dougie Squire <42455466+dougiesquire@users.noreply.github.com>

--- src/framework/MOM_restart.F90
+++ src/framework/MOM_restart.F90
@@ -2106,7 +2106,7 @@ function open_restart_units(filename, directory, G, CS, IO_handles, file_paths, 
   integer :: nf              ! The number of files that have been found so far
   integer :: m, length
   logical :: still_looking   ! If true, the code is still looking for automatically named files
-  logical :: fexists         ! True if a file has been found
+  logical :: fexists, fexists_decomp ! True if a file has been found
   character(len=32) :: filename_appendix = '' ! Filename appendix for ensemble runs
   character(len=80) :: restartname
 
@@ -2183,9 +2183,17 @@ function open_restart_units(filename, directory, G, CS, IO_handles, file_paths, 
     else
       filepath = trim(directory)//trim(fname)
       inquire(file=filepath, exist=fexists)
-      if (.not. fexists) filepath = trim(filepath)//".nc"
+      if (.not.fexists .and. CS%parallel_restartfiles) &
+        fexists_decomp = file_exists(filepath, G%Domain)
 
-      inquire(file=filepath, exist=fexists)
+      ! If not found, try with ".nc" extension
+      if (.not.(fexists .or. fexists_decomp)) then
+        filepath = trim(filepath)//".nc"
+        inquire(file=filepath, exist=fexists)
+        if (.not.fexists .and. CS%parallel_restartfiles) &
+              fexists_decomp = file_exists(filepath, G%Domain)
+      endif
+
       if (fexists) then
         nf = nf + 1
         if (present(IO_handles)) &
@@ -2195,6 +2203,14 @@ function open_restart_units(filename, directory, G, CS, IO_handles, file_paths, 
         if (present(file_paths)) file_paths(nf) = filepath
         if (is_root_pe() .and. (present(IO_handles))) &
           call MOM_error(NOTE, "MOM_restart: MOM run restarted using : "//trim(filepath))
+      elseif (fexists_decomp) then
+        nf = nf + 1
+        if (present(IO_handles)) &
+          call IO_handles(nf)%open(trim(filepath), READONLY_FILE, MOM_domain=G%Domain)
+        if (present(global_files)) global_files(nf) = .false.
+        if (present(file_paths)) file_paths(nf) = filepath
+        if (is_root_pe() .and. present(IO_handles)) &
+            call MOM_error(NOTE, "MOM_restart: MOM run restarted using decomposed fileset: "//trim(filepath))
       else
         if (present(IO_handles)) &
           call MOM_error(WARNING, "MOM_restart: Unable to find restart file : "//trim(filepath))
