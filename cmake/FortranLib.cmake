e6fe513e319030549ae41eea46618715818587d8
Dougie Squire <42455466+dougiesquire@users.noreply.github.com>
2025-12-16 13:51:34 +1100

    CMake: Make access3 and solo ocean builds non-exclusive (#36, #37 and #53)

--- cmake/FortranLib.cmake
+++ cmake/FortranLib.cmake
@@ -1,10 +1,20 @@
 # Copyright ACCESS-NRI and contributors. See the top-level LICENSE file for details.
 
-function(add_fortran_library LIB MOD_DIR)
-    add_library(${LIB} ${ARGN})
+function(add_fortran_target TARGET_NAME TYPE MOD_DIR)
+  if(${TYPE} STREQUAL "LIB")
+      add_library(${TARGET_NAME} ${ARGN})
+  elseif(${TYPE} STREQUAL "EXE")
+      add_executable(${TARGET_NAME} ${ARGN})
+  else()
+      message(FATAL_ERROR "Invalid TYPE '${TYPE}' for add_fortran_target. Use LIB or EXE.")
+  endif()
 
-    get_target_property(LIB_DIR ${LIB} BINARY_DIR)
-    set_target_properties(${LIB} PROPERTIES Fortran_MODULE_DIRECTORY ${LIB_DIR}/${MOD_DIR})
+  # Set Fortran module directory
+  get_target_property(TGT_BUILD_DIR ${TARGET_NAME} BINARY_DIR)
+  set_target_properties(${TARGET_NAME} PROPERTIES
+      Fortran_MODULE_DIRECTORY "${TGT_BUILD_DIR}/${MOD_DIR}"
+  )
 
-    target_include_directories(${LIB} INTERFACE "$<BUILD_INTERFACE:${LIB_DIR}/${MOD_DIR}>")
-  endfunction(add_fortran_library)
+  # Add the module directory to the include path
+  target_include_directories(${TARGET_NAME} INTERFACE "$<BUILD_INTERFACE:${TGT_BUILD_DIR}/${MOD_DIR}>")
+endfunction()
